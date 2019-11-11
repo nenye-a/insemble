1526ad12badd62d9b676f5a0faa4b01929c99384
Colin Webb <colin@Colins-MacBook-Pro.local>
2019-11-10 16:20:23 -0800

    updated tenant page. Added landlord page

--- web-app/src/layouts/index.js
+++ web-app/src/layouts/index.js
@@ -1,5 +1,6 @@
 import DefaultLayout from "./Default";
 import HeaderNavigation from "./HeaderNavigation";
 import IconSidebar from "./IconSidebar";
+import LandlordLayout from "./Landlord";
 
-export { DefaultLayout, HeaderNavigation, IconSidebar };
+export { DefaultLayout, HeaderNavigation, IconSidebar, LandlordLayout };
