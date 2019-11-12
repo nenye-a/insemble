f7bd495829e6ba126a0504f4568820492bb9ac64
Nenye <57047007+nenye-a@users.noreply.github.com>
2019-11-11 16:22:30 -0800

    latest and greatest back-end/front-end combo

--- insembleapp/backend/insembleapp/urls.py
+++ insembleapp/backend/insembleapp/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url  # noqa
+from django.urls import include, path
 from django.contrib import admin
 from django.views.generic import TemplateView
 
@@ -10,7 +11,7 @@ from .api import PairedLocationViewSet # RetailerViewSet, LocationViewSet,
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 # TODO: register location and retailer api once necessary
-router.register(r'pairedlocation', PairedLocationViewSet, basename='paired')
+router.register(r'api/pairs', PairedLocationViewSet, basename='pair')
 
 
 urlpatterns = [
@@ -24,5 +25,6 @@ urlpatterns = [
     url(r'^(?:.*)/?$', TemplateView.as_view(template_name='insembleapp/index.html'), name='home'),
     # url(r'^$', TemplateView.as_view(template_name='exampleapp/itworks.html'), name='home'),
 
-    url(r'^', include(router.urls)),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
