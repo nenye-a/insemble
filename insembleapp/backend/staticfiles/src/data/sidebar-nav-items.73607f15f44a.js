e812519dc28488cb010a7cfffaf88525ed0da97e
Nenye <57047007+nenye-a@users.noreply.github.com>
2019-11-24 11:41:18 -0800

    updated and merged with origin

---
+++ insembleapp/backend/staticfiles/src/data/sidebar-nav-items.73607f15f44a.js
@@ -0,0 +1,32 @@
+export default function () {
+  return [{
+    title: 'Dashboard', 
+    items: [{
+      title: 'Explore',
+      to: '/explore',
+      htmlBefore: '<i class="material-icons">&#xE917;</i>',
+      htmlAfter: '',
+    }, {
+      title: 'Matches',
+      to: '/matches',
+      htmlBefore: '<i class="material-icons">&#xE8D1;</i>',
+      htmlAfter: '',
+    },
+    {
+      title: "My Spaces",
+      htmlBefore: '<i class="material-icons">view_module</i>',
+      items: [
+        {
+          title: "Main",
+          to: "/tenant-deep-dive"
+        }
+      ]
+    },
+    {
+      title: 'Pending Deals',
+      to: '/no-deals',
+      htmlBefore: '<i class="material-icons">edit</i>',
+      htmlAfter: '',
+    }],
+  }];
+}
