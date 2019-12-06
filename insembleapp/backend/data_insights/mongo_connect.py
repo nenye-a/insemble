69d06ef21b52be489a4dda686abb5ce5351416e8
Nenye <57047007+nenye-a@users.noreply.github.com>
2019-12-06 14:15:46 -0800

    removed node-files from tracking & updated settings to hide env variables

--- insembleapp/backend/data_insights/mongo_connect.py
+++ insembleapp/backend/data_insights/mongo_connect.py
@@ -2,11 +2,16 @@ from pymongo import MongoClient
 from decouple import config
 import urllib
 
+import os
+from django.conf import settings
+
 #### TODO: keep key secret by using environment variables
 
+MONGO_PASS = settings.MONGO_DB_PASS
 
 class Connect(object):
     @staticmethod
     def get_connection():
+        # mongo_uri = "DELETED_MONGODB_URL"
         mongo_uri = "DELETED_MONGODB_URL"
         return MongoClient(mongo_uri)
