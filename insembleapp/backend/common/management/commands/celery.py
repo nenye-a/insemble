4a2b4ec8762961b4421f2cbea37cb63df7f06f97
Nenye <57047007+nenye-a@users.noreply.github.com>
2019-12-05 18:54:28 -0800

    connected celery to redis pending implementation

--- insembleapp/backend/common/management/commands/celery.py
+++ insembleapp/backend/common/management/commands/celery.py
@@ -11,6 +11,9 @@ import psutil
 
 def restart_celery():
     for proc in psutil.process_iter():
+
+        if proc.status() == 'zombie': # skip pesky zombie processes
+            continue
         if proc.username() != getpass.getuser():  # skip processes not owned by user
             continue
         if proc.name() != 'celery':
@@ -22,6 +25,7 @@ def restart_celery():
         celery_proc = proc  # found parent celery process
         celery_proc.terminate()
         break
+
     cmd = "celery worker -A insembleapp -l INFO"
     psutil.Popen(shlex.split(cmd), stdout=PIPE)
 
