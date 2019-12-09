60fa0e5e65cb2f428ec6636fcced3386585de250
Nenye <57047007+nenye-a@users.noreply.github.com>
2019-12-08 23:24:22 -0800

    Updated readme & added backend clarifying comments

--- insembleapp/backend/feedback/serializers.py
+++ insembleapp/backend/feedback/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Feedback
 from django.contrib.auth import authenticate
 
+# Serializer for storing feedback
 class FeedbackSerializer(serializers.ModelSerializer):
 
     class Meta:
