f1592e1e510258201b4063c565da44713aa21cc8
Simon <sstur@me.com>
2019-12-17 14:44:49 -0800

    Refactor to remove deprecated componentWillMount

--- insembleapp/frontend/src/components/layout/HeaderNavbar/HeaderNavbar.js
+++ insembleapp/frontend/src/components/layout/HeaderNavbar/HeaderNavbar.js
@@ -1,57 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col, Nav, Collapse } from 'shards-react';
 
 import { Store } from '../../../flux';
 import MenuItem from './MenuItem';
 
-class HeaderNavbar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      menuVisible: false,
+function HeaderNavbar(props) {
+  let [menuVisible, setMenuVisible] = useState(false);
+  useEffect(() => {
+    let onChange = () => {
+      setMenuVisible(Store.getMenuState());
     };
-
-    this.onChange = this.onChange.bind(this);
-  }
-
-  componentWillMount() {
-    Store.addChangeListener(this.onChange);
-  }
-
-  componentWillUnmount() {
-    Store.removeChangeListener(this.onChange);
-  }
-
-  onChange() {
-    this.setState({
-      ...this.state,
-      menuVisible: Store.getMenuState(),
-    });
-  }
-
-  render() {
-    const { items } = this.props;
-    return (
-      <Collapse
-        className="header-navbar d-lg-flex p-0 bg-white border-top"
-        open={this.state.menuVisible}
-      >
-        <Container>
-          <Row>
-            <Col>
-              <Nav tabs className="border-0 flex-column flex-lg-row">
-                {items.map((item, idx) => (
-                  <MenuItem key={idx} item={item} />
-                ))}
-              </Nav>
-            </Col>
-          </Row>
-        </Container>
-      </Collapse>
-    );
-  }
+    Store.addChangeListener(onChange);
+    return () => {
+      Store.removeChangeListener(onChange);
+    };
+  }, []);
+  const { items } = props;
+  return (
+    <Collapse className="header-navbar d-lg-flex p-0 bg-white border-top" open={menuVisible}>
+      <Container>
+        <Row>
+          <Col>
+            <Nav tabs className="border-0 flex-column flex-lg-row">
+              {items.map((item, idx) => (
+                <MenuItem key={idx} item={item} />
+              ))}
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+    </Collapse>
+  );
 }
 
 HeaderNavbar.propTypes = {
