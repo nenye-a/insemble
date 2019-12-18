9b742ee8179277f164ae8eb4cedd663948c1f2d2
Simon Sturmer <sstur@me.com>
2019-12-17 21:43:02 -0800

    Remove deprecated method `componentWillMount` (#36)
    * Refactor to remove deprecated componentWillMount
    
    * .refs -> ._refs

--- insembleapp/frontend/src/views/MapContainerDeepDive.js
+++ insembleapp/frontend/src/views/MapContainerDeepDive.js
@@ -1,128 +1,116 @@
 /* global google */
 import React from 'react';
-import { Row, Container, Col } from 'shards-react';
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-  Circle,
-  InfoWindow,
-} from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, Circle } from 'react-google-maps';
 import { Redirect } from 'react-router-dom';
 
-import HeatMapLayer from 'react-google-DELETED_BASE64_STRING';
 import SearchBox from 'react-google-maps/lib/components/places/SearchBox';
 const _ = require('lodash');
 
 class MapWithAMarkerClusterer extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    redirect: false,
+    isMarkerShown: false,
+    markerPosition: null,
+    markerAddress: null,
+    //bounds: new google.maps.LatLngBounds(new google.maps.LatLng(33.7036519, -118.6681759), new google.maps.LatLng(34.3373061, -118.1552891)),
+    bounds: null,
+    center: {
+      lat: this.props.markers.lat,
+      lng: this.props.markers.lng,
+    },
+    markers: [],
+  };
 
-    this.state = {
-      redirect: false,
-      isMarkerShown: false,
-      markerPosition: null,
-      markerAddress: null,
-    };
-    this.handleMarkerClustererClick = this.handleMarkerClustererClick.bind(this);
-    this.handleMarkerClick = this.handleMarkerClick.bind(this);
-  }
+  _refs = {};
 
-  componentWillMount() {
-    const refs = {};
+  onMapMounted = (ref) => {
+    let refs = this._refs;
+    refs.map = ref;
+    if (refs.map) {
+      this.setState({
+        bounds: refs.map.getBounds(),
+        center: refs.map.getCenter(),
+      });
+    }
+    console.log('state set');
+  };
+
+  onBoundsChanged = () => {
+    let refs = this._refs;
+    this.setState({
+      bounds: refs.map.getBounds(),
+      center: refs.map.getCenter(),
+    });
+  };
+
+  onSearchBoxMounted = (ref) => {
+    this._refs.searchBox = ref;
+  };
+
+  onPlacesChanged = () => {
+    let refs = this._refs;
+    const places = refs.searchBox.getPlaces();
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    const nextMarkers = places.map((place) => ({
+      position: place.geometry.location,
+    }));
+    const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
 
     this.setState({
-      //bounds: new google.maps.LatLngBounds(new google.maps.LatLng(33.7036519, -118.6681759), new google.maps.LatLng(34.3373061, -118.1552891)),
-      bounds: null,
-      center: {
-        lat: this.props.markers.lat,
-        lng: this.props.markers.lng,
-      },
-      markers: [],
-      onMapMounted: (ref) => {
-        refs.map = ref;
-        if (refs.map) {
-          this.setState({
-            bounds: refs.map.getBounds(),
-            center: refs.map.getCenter(),
-          });
-        }
-        console.log('state set');
-      },
-      onBoundsChanged: () => {
-        this.setState({
-          bounds: refs.map.getBounds(),
-          center: refs.map.getCenter(),
-        });
-      },
-      onSearchBoxMounted: (ref) => {
-        refs.searchBox = ref;
-      },
-      onPlacesChanged: () => {
-        const places = refs.searchBox.getPlaces();
-        const bounds = new google.maps.LatLngBounds();
-
-        places.forEach((place) => {
-          if (place.geometry.viewport) {
-            bounds.union(place.geometry.viewport);
-          } else {
-            bounds.extend(place.geometry.location);
-          }
-        });
-        const nextMarkers = places.map((place) => ({
-          position: place.geometry.location,
-        }));
-        const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
-
-        this.setState({
-          center: nextCenter,
-          markers: nextMarkers,
-        });
-        refs.map.fitBounds(bounds);
-      },
-      onMapClick: (e) => {
-        // console.log(e.latLng.lat().toString().split(".").join(""))
-        // console.log("stringed")
-        // console.log('api/location/lat='+e.latLng.lat().toString().split(".").join("")+'&lng='+e.latLng.lng().toString().split(".").join(""))
-        fetch(
-          'api/location/lat=' +
-            e.latLng
-              .lat()
-              .toString()
-              .split('.')
-              .join('') +
-            '&lng=' +
-            e.latLng
-              .lng()
-              .toString()
-              .split('.')
-              .join('') +
-            '&radius=1'
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            // console.log(data)
-            // console.log("request made")
-            this.setState({ markerPosition: e.latLng, isMarkerShown: true, marker: data });
-          });
-
-        console.log(this.state.marker);
-        // console.log("pressed")
-      },
+      center: nextCenter,
+      markers: nextMarkers,
     });
-  }
+    refs.map.fitBounds(bounds);
+  };
 
-  handleMarkerClustererClick(markerClusterer) {
-    const clickedMarkers = markerClusterer.getMarkers();
-    // console.log(`Current clicked markers length: ${clickedMarkers.length}`)
-    // console.log(clickedMarkers)
-  }
+  onMapClick = (e) => {
+    // console.log(e.latLng.lat().toString().split(".").join(""))
+    // console.log("stringed")
+    // console.log('api/location/lat='+e.latLng.lat().toString().split(".").join("")+'&lng='+e.latLng.lng().toString().split(".").join(""))
+    fetch(
+      'api/location/lat=' +
+        e.latLng
+          .lat()
+          .toString()
+          .split('.')
+          .join('') +
+        '&lng=' +
+        e.latLng
+          .lng()
+          .toString()
+          .split('.')
+          .join('') +
+        '&radius=1'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        // console.log("request made")
+        this.setState({ markerPosition: e.latLng, isMarkerShown: true, marker: data });
+      });
 
-  handleMarkerClick(marker) {
+    console.log(this.state.marker);
+    // console.log("pressed")
+  };
+
+  // handleMarkerClustererClick = (markerClusterer) => {
+  //   const clickedMarkers = markerClusterer.getMarkers();
+  //   console.log(`Current clicked markers length: ${clickedMarkers.length}`)
+  //   console.log(clickedMarkers)
+  // };
+
+  handleMarkerClick = (marker) => {
     // console.log('Go to the marker post page')
     this.setState({ redirect: true, marker: marker });
-  }
+  };
 
   handleSearchClick(marker) {
     // console.log(marker)
@@ -149,15 +137,15 @@ class MapWithAMarkerClusterer extends React.Component {
 
     return (
       <GoogleMap
-        ref={this.state.onMapMounted}
+        ref={this.onMapMounted}
         defaultZoom={13}
         defaultCenter={{ lat: lat, lng: lng }}
         defaultOptions={{
           minZoom: 7,
           mapTypeControl: false,
         }}
-        onClick={this.state.onMapClick}
-        onBoundsChanged={this.state.onBoundsChanged}
+        onClick={this.onMapClick}
+        onBoundsChanged={this.onBoundsChanged}
       >
         {this.renderRedirect()}
         {/* icons from https://sites.google.com/site/gmapsdevelopment/ */}
@@ -210,10 +198,10 @@ class MapWithAMarkerClusterer extends React.Component {
         />
 
         <SearchBox
-          ref={this.state.onSearchBoxMounted}
+          ref={this.onSearchBoxMounted}
           bounds={this.state.bounds}
           controlPosition={google.maps.ControlPosition.TOP}
-          onPlacesChanged={this.state.onPlacesChanged}
+          onPlacesChanged={this.onPlacesChanged}
         >
           <input
             type="text"
