231456ff3730516ac2878a3287a970ba3e608fba
Simon <sstur@me.com>
2019-12-24 10:26:37 -0800

    Google Places integration

---
+++ insembleapp/frontend/src/constants/googleMaps.ts
@@ -0,0 +1,7 @@
+const MAPS_API_KEY = 'AIzaSyCJjsXi3DbmlB1soI9kHzANRqVkiWj3P2U';
+
+export const MAPS_URI = `https://maps.googleapis.com/maps/api/js?key=${MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places,visualization`;
+
+export const MAPS_IFRAME_URL_SEARCH = `https://www.google.com/maps/embed/v1/search?key=${MAPS_API_KEY}`;
+
+export const MAPS_IFRAME_URL_PLACE = `https://www.google.com/maps/embed/v1/place?key=${MAPS_API_KEY}`;
--- insembleapp/frontend/src/utils/urls.js
+++
@@ -1,2 +0,0 @@
-export const MAP_IFRAME_URL =
-  'https://www.google.com/maps/embed/v1/search?key=AIzaSyCJjsXi3DbmlB1soI9kHzANRqVkiWj3P2U&q=';
