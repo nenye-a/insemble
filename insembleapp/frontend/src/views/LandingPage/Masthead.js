d27c6b6d19a2f7655c7036bf398e43ecc9e732c8
Simon Sturmer <sstur@me.com>
2019-12-23 18:30:39 -0800

    Begin new landing page (#44)

---
+++ insembleapp/frontend/src/views/LandingPage/Masthead.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BackgroundContainer = styled.div`
+  background-image: url('https://insemble-photos.s3.us-east-2.amazonaws.com/interactive-landing-background.jpg');
+  background-size: cover;
+  background-position-x: center;
+  background-position-y: center;
+  background-repeat: no-repeat;
+`;
+
+const ContentContainer = styled.div`
+  background-color: rgba(32, 32, 32, 0.7);
+  min-height: 100vh;
+  padding-top: 72px;
+`;
+
+const Content = styled.div`
+  max-width: 740px;
+  margin: 0 auto;
+`;
+
+export default (props) => {
+  return (
+    <BackgroundContainer>
+      <ContentContainer>
+        <Content>{props.children}</Content>
+      </ContentContainer>
+    </BackgroundContainer>
+  );
+};
