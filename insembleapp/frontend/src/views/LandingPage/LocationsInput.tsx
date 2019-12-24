e7f3558a4f61e2b5ba697c32a5aa60f7913c2e7c
Simon <sstur@me.com>
2019-12-24 10:26:37 -0800

    Google Places integration

---
+++ insembleapp/frontend/src/views/LandingPage/LocationsInput.tsx
@@ -0,0 +1,43 @@
+import React, { createRef, useEffect, useRef, useCallback } from 'react';
+import TextInput from './TextInput';
+
+type PlaceResult = google.maps.places.PlaceResult;
+
+type Props = {
+  placeholder: string;
+  buttonText: string;
+  onSubmit: (place: PlaceResult) => void;
+};
+
+function LocationsInput(props: Props) {
+  let { placeholder, buttonText, onSubmit } = props;
+  let inputRef = createRef<HTMLInputElement>();
+  let selectedPlace = useRef<PlaceResult>();
+  let submitHandler = useCallback(() => {
+    if (selectedPlace.current) {
+      onSubmit(selectedPlace.current);
+    }
+  }, [onSubmit]);
+  useEffect(() => {
+    if (inputRef.current) {
+      let autocomplete = new window.google.maps.places.Autocomplete(inputRef.current);
+      let listener = autocomplete.addListener('place_changed', () => {
+        let place = autocomplete.getPlace();
+        selectedPlace.current = place;
+      });
+      return () => {
+        listener.remove();
+      };
+    }
+  }, []);
+  return (
+    <TextInput
+      ref={inputRef}
+      placeholder={placeholder}
+      buttonText={buttonText}
+      onSubmit={submitHandler}
+    />
+  );
+}
+
+export default LocationsInput;
