0d8056241340fd98a54d6a10c05b9cfcad810007
Simon <sstur@me.com>
2019-12-26 13:59:14 -0800

    Use Google API key from .env

--- insembleapp/frontend/src/constants/googleMaps.ts
+++ insembleapp/frontend/src/constants/googleMaps.ts
@@ -1,4 +1,4 @@
-const MAPS_API_KEY = 'AIzaSyCJjsXi3DbmlB1soI9kHzANRqVkiWj3P2U';
+const MAPS_API_KEY = process.env.GOOG_KEY || 'AIzaSyCJjsXi3DbmlB1soI9kHzANRqVkiWj3P2U';
 
 export const MAPS_URI = `https://maps.googleapis.com/maps/api/js?key=${MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places,visualization`;
 
