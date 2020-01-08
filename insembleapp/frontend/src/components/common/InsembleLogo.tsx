2eb67c9251010077ff816fb6c2eef7dda1e93b38
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-01-08 13:03:33 +0700

    add logo

---
+++ insembleapp/frontend/src/components/common/InsembleLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../../assets/images/insemble-logo.svg';
+
+export default function InsembleLogo() {
+  return <Image src={logo} alt="Insemble" />;
+}
+
+const Image = styled('img')`
+  max-height: 100%;
+`;
