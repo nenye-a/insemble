4be32d4a925ba720470674545ec9638564ecfc99
Simon <sstur@me.com>
2020-01-09 09:02:09 -0800

    Refactor TouchableOpacity

--- insembleapp/frontend/src/core-ui/TouchableOpacity.tsx
+++ insembleapp/frontend/src/core-ui/TouchableOpacity.tsx
@@ -1,19 +1,65 @@
-import React, { ComponentProps } from 'react';
-import styled from 'styled-components';
+import React, { ComponentProps, useCallback } from 'react';
+import styled, { css } from 'styled-components';
 
-type TouchableOpacityProps = ComponentProps<'a'>;
+type PressHandler = () => void;
 
-export const Container = styled('div')`
+type LinkProps = Omit<ComponentProps<'a'>, 'href'> & {
+  href: string;
+};
+
+type DivProps = ComponentProps<'div'>;
+
+type TouchableProps = (LinkProps | DivProps) & { onPress: PressHandler };
+
+function Touchable(props: TouchableProps) {
+  let { onPress, ...otherProps } = props;
+  let onClick = useCallback(
+    (event) => {
+      if (onPress) {
+        event.preventDefault();
+        onPress();
+      }
+    },
+    [onPress]
+  );
+  return 'href' in otherProps ? (
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
+    <a {...otherProps} onClick={onClick} />
+  ) : (
+    <div {...otherProps} onClick={onClick} tabIndex={0} />
+  );
+}
+
+let linkStyles = css`
+  background-color: rgba(0, 0, 0, 0);
+  color: inherit;
+  text-align: inherit;
+  font: inherit;
+  list-style: none;
+  text-decoration: none;
+`;
+
+export default styled(Touchable)`
+  align-items: stretch;
+  box-sizing: border-box;
+  display: flex;
+  flex-basis: auto;
+  flex-direction: column;
+  flex-shrink: 0;
+  margin: 0;
+  min-height: 0;
+  min-width: 0;
+  padding: 0;
+  position: relative;
+  z-index: 0;
+  border: 0 solid black;
+  border-image: initial;
+  touch-action: manipulation;
+  cursor: pointer;
+  user-select: none;
+  transition-property: opacity;
+  ${(props) => ('href' in props ? linkStyles : '')}
   &:hover {
     opacity: 0.9;
   }
 `;
-
-export default function TouchableOpacity(props: TouchableOpacityProps) {
-  let { children, ...otherProps } = props;
-  return (
-    <Container>
-      <a {...otherProps}>{children}</a>
-    </Container>
-  );
-}
