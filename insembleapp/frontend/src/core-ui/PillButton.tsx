f48273425d1b98ac104409f0005a2495dec2aff6
Katherine Philip <kurerunoshikaku@yahoo.com>
2020-01-09 14:16:31 +0700

    Make PillButton a core-ui

---
+++ insembleapp/frontend/src/core-ui/PillButton.tsx
@@ -0,0 +1,50 @@
+import React, { ComponentProps } from 'react';
+import styled, {css} from 'styled-components';
+import {TEXT_COLOR, THEME_COLOR, WHITE} from '../constants/colors';
+
+type PillButtonProps = {
+    primary?: boolean;
+}
+
+const PillButton = styled(DefaultButton)`
+  flex: 0 1 auto;
+  margin: 0.35rem 0.25rem;
+  padding: 0.5rem 0.5rem;
+  line-height: 1;
+
+  text-align: center;
+
+  /* So the size doesn't slightly change when purple border shows up when highlighted */
+  border: 1px solid;
+  border-color: ${WHITE};
+
+  border-radius: 0.375rem;
+  box-shadow: 0 0 0.35rem rgba(0, 0, 0, 0.15);
+
+  background: ${WHITE};
+  color: ${TEXT_COLOR};
+
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  &:hover {
+    color: ${THEME_COLOR};
+    border-color: ${THEME_COLOR};
+  }
+
+  ${(props: PillButtonProps) => props.primary && css`
+  background: ${THEME_COLOR};
+  color: ${WHITE};
+
+  border-color: ${THEME_COLOR};
+
+  &:hover {
+    color: ${WHITE};
+  }`}
+`;
+
+function DefaultButton(props: ComponentProps<'button'>) {
+    return <button {...props} type="button" />;
+}
+
+export default PillButton;
--- insembleapp/frontend/src/views/LandingPage/CategoryTagList.tsx
+++ insembleapp/frontend/src/views/LandingPage/CategoryTagList.tsx
@@ -1,6 +1,7 @@
 import React, { ComponentProps } from 'react';
 import styled from 'styled-components';
-import {TEXT_COLOR, THEME_COLOR, WHITE} from '../../constants/colors';
+
+import {PillButton} from "../../core-ui";
 
 type Props = {
   allCategories: Array<string>;
@@ -9,52 +10,10 @@ type Props = {
   onSelect: (tag: string) => void;
 };
 
-function Button(props: ComponentProps<'button'>) {
-  return <button {...props} type="button" />;
-}
-
 const TagContainer = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: left;
-`;
-
-const PillButton = styled(Button)`
-  flex: 0 1 auto;
-  margin: 0.35rem 0.25rem;
-  padding: 0.5rem 0.5rem;
-  line-height: 1;
-
-  text-align: center;
-
-  /* So the size doesn't slightly change when purple border shows up when highlighted */
-  border: 1px solid;
-  border-color: ${WHITE};
-
-  border-radius: 0.375rem;
-  box-shadow: 0 0 0.35rem rgba(0, 0, 0, 0.15);
-
-  background: ${WHITE};
-  color: ${TEXT_COLOR};
-
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-
-  &:hover {
-    color: ${THEME_COLOR};
-    border-color: ${THEME_COLOR};
-  }
-`;
-
-const SelectedPill = styled(PillButton)`
-  background: ${THEME_COLOR};
-  color: ${WHITE};
-
-  border-color: ${THEME_COLOR};
-
-  &:hover {
-    color: ${WHITE};
-  }
 `;
 
 export default (props: Props) => {
@@ -66,9 +25,9 @@ export default (props: Props) => {
       <TagContainer>
         {selectedCategories.map((tag, idx) => {
           return (
-            <SelectedPill key={idx} onClick={() => onUnselect(tag)}>
+            <PillButton primary key={idx} onClick={() => onUnselect(tag)}>
               {tag}
-            </SelectedPill>
+            </PillButton>
           );
         })}
       </TagContainer>
