c3d96000f59a5b854014067218e68575f17eb4ca
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-01-10 16:19:11 +0800

    Core UI Text & Card (#68)

--- insembleapp/frontend/src/constants/theme.ts
+++ insembleapp/frontend/src/constants/theme.ts
@@ -1,5 +1,9 @@
 // TODO: Use the correct font-family to match the design.
-export const FONT_FAMILY_NORMAL = 'inherit';
+export const FONT_FAMILY_NORMAL = 'Avenir';
 
+export const FONT_SIZE_XSMALL = '9px';
+export const FONT_SIZE_SMALL = '12px';
 export const FONT_SIZE_NORMAL = '14px';
 export const FONT_SIZE_LARGE_INPUT = '16px';
+
+export const DEFAULT_BORDER_RADIUS = '4px';
