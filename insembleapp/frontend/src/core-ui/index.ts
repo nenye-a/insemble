f7f5ae3e24121f01af4af63f78e7804c3db9c7a8
Simon Sturmer <sstur@me.com>
2020-01-10 00:06:14 -0800

    Support prop flex on View (#70)

--- insembleapp/frontend/src/core-ui/index.ts
+++ insembleapp/frontend/src/core-ui/index.ts
@@ -4,4 +4,3 @@ export { default as Text } from './Text';
 export { default as TextInputWithLabel } from './TextInputWithLabel';
 export { default as TouchableOpacity } from './TouchableOpacity';
 export { default as View } from './View';
-export { default as ViewFlex } from './ViewFlex';
