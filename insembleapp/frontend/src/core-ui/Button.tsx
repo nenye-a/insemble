8046ac116892eb262f32ebcc847cb50ba0332410
Simon <sstur@me.com>
2020-01-12 18:14:35 -0800

    Use TouchableOpacity for Button

--- insembleapp/frontend/src/core-ui/Button.tsx
+++ insembleapp/frontend/src/core-ui/Button.tsx
@@ -1,27 +1,37 @@
 import React, { ComponentProps } from 'react';
 import styled from 'styled-components';
 import { THEME_COLOR } from '../constants/colors';
-import { FONT_FAMILY_NORMAL, FONT_SIZE_NORMAL } from '../constants/theme';
+import TouchableOpacity from './TouchableOpacity';
+import Text from './Text';
 
-type ButtonProps = ComponentProps<'button'>;
+type TextProps = ComponentProps<typeof Text>;
 
-function Button(props: ButtonProps) {
-  return <button type="button" {...props} />;
+type Props = ComponentProps<typeof TouchableOpacity> & {
+  text: string;
+  textProps: TextProps;
+};
+
+function Button(props: Props) {
+  let { text, textProps, ...otherProps } = props;
+  return (
+    <TouchableOpacity forwardedAs="button" type="button" {...otherProps}>
+      <Text as="span" color="white" {...textProps}>
+        {text}
+      </Text>
+    </TouchableOpacity>
+  );
 }
 
-// TODO: Should we style this with a pressed state similar to TouchableOpacity?
 export default styled(Button)`
-  color: #fff;
-  border: none;
   background-color: ${THEME_COLOR};
-  box-shadow: none;
   border-radius: 4px;
   height: 36px;
-  font-family: ${FONT_FAMILY_NORMAL};
-  font-size: ${FONT_SIZE_NORMAL};
-  padding-left: 12px;
-  padding-right: 12px;
+  justify-content: center;
+  padding: 0 12px;
   &:hover {
     opacity: 0.9;
+  }
+  &:active {
+    opacity: 0.5;
   }
 `;
