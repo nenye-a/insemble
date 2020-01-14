415ff689ed15d9485653600d727adcff588069a3
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-01-14 14:33:49 +0800

    [Core UI] Dropdown & ClickAway (#76)

--- insembleapp/frontend/src/core-ui/index.ts
+++ insembleapp/frontend/src/core-ui/index.ts
@@ -1,5 +1,7 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as ClickAway } from './ClickAway';
+export { default as Dropdown } from './Dropdown';
 export { default as PillButton } from './PillButton';
 export { default as Text } from './Text';
 export { default as TextInputWithLabel } from './TextInputWithLabel';
