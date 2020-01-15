3bd5c6e8119c4cf33860f4bb6e6f5f2cf82def6a
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-01-15 11:04:05 +0700

    button secondary mode

--- insembleapp/frontend/src/core-ui/Button.tsx
+++ insembleapp/frontend/src/core-ui/Button.tsx
@@ -1,6 +1,7 @@
 import React, { ComponentProps } from 'react';
-import styled from 'styled-components';
-import { THEME_COLOR } from '../constants/colors';
+import styled, { css } from 'styled-components';
+import { THEME_COLOR, WHITE } from '../constants/colors';
+import { BUTTON_BORDER_COLOR, TEXT_COLOR } from '../constants/colors';
 import TouchableOpacity from './TouchableOpacity';
 import Text from './Text';
 
@@ -9,6 +10,7 @@ type TextProps = ComponentProps<typeof Text>;
 type Props = ComponentProps<typeof TouchableOpacity> & {
   text: string;
   textProps: TextProps;
+  mode?: 'primary' | 'secondary';
 };
 
 function Button(props: Props) {
@@ -22,12 +24,21 @@ function Button(props: Props) {
   );
 }
 
-export default styled(Button)`
+export default styled(Button)<Props>`
   background-color: ${THEME_COLOR};
   border-radius: 4px;
   height: 36px;
   justify-content: center;
   padding: 0 12px;
+  ${(props) =>
+    props.mode === 'secondary' &&
+    css`
+      background-color: ${WHITE};
+      border: 0.5px solid ${BUTTON_BORDER_COLOR};
+      ${Text} {
+        color: ${TEXT_COLOR};
+      }
+    `}
   &:hover {
     opacity: 0.9;
   }
