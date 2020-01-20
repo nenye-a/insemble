0d65d58f8e8eb2b56f93e3c61d9c64ec2b00dd57
Filbert <39455112+FilbertAW@users.noreply.github.com>
2020-01-20 14:34:17 +0700

    Add Location Detail (#83)
    * Add Location Detail
    
    * Fix Based On Review

---
+++ insembleapp/frontend/src/components/location-detail/LocationDetail.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Card, View, Text, TouchableOpacity } from '../../core-ui';
+import styled from 'styled-components';
+import { THEME_COLOR } from '../../constants/colors';
+import { FONT_SIZE_SMALL, FONT_WEIGHT_MEDIUM } from '../../constants/theme';
+import SvgClose from '../icons/close';
+
+type Props = {
+  visible: boolean;
+  title: string;
+  subTitle: string;
+  population: string;
+  income: string;
+  age: number;
+  gender: string;
+  ethnicity: Array<string>;
+  onSeeMore: () => void;
+};
+
+export default function LocationDetail(props: Props) {
+  let { population, income, age, gender, ethnicity, title, onSeeMore, visible, subTitle } = props;
+  let leftText = [
+    '3 Mile Population:',
+    'Median Income:',
+    'Average Age:',
+    'Gender:',
+    'Top Ethnicity:',
+  ];
+  let rightText = [population, income, age + 'years', gender, ethnicity.join(', ')];
+  return visible ? (
+    <Container
+      mode={'with-title'}
+      title={title}
+      subTitle={subTitle}
+      titleProps={{ fontWeight: FONT_WEIGHT_MEDIUM }}
+    >
+      <ContentContainer>
+        <LeftColumn>
+          {leftText.map((line) => (
+            <SmallText>{line}</SmallText>
+          ))}
+        </LeftColumn>
+        <RightColumn>
+          {rightText.map((line) => (
+            <RightColumnText>{line}</RightColumnText>
+          ))}
+          <TouchableOpacity onPress={onSeeMore}>
+            <SeeMore>Click again to see more</SeeMore>
+          </TouchableOpacity>
+        </RightColumn>
+      </ContentContainer>
+    </Container>
+  ) : null;
+}
+
+const Container = styled(Card)`
+  width: 300px;
+  height: auto;
+`;
+const ContentContainer = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  padding: 0 10px 0 5px;
+`;
+const LeftColumn = styled(View)`
+  flex: 1;
+  align-items: flex-start;
+`;
+const RightColumn = styled(View)`
+  flex: 1;
+  align-items: flex-end;
+  text-align: right;
+`;
+const RightColumnText = styled(Text)`
+  color: ${THEME_COLOR};
+  font-size: ${FONT_SIZE_SMALL};
+  margin: 10px 0 0 0;
+`;
+const SmallText = styled(Text)`
+  font-size: ${FONT_SIZE_SMALL};
+  margin: 10px 0 0 0;
+`;
+const SeeMore = styled(Text)`
+  font-size: ${FONT_SIZE_SMALL};
+  font-style: italic;
+  color: ${THEME_COLOR};
+  margin: 10px 0 5px 0;
+`;
--- insembleapp/frontend/src/core-ui/Card.tsx
+++ insembleapp/frontend/src/core-ui/Card.tsx
@@ -4,6 +4,7 @@ import View from './View';
 import Text from './Text';
 import { DEFAULT_BORDER_RADIUS } from '../constants/theme';
 import { THEME_COLOR, WHITE } from '../constants/colors';
+import { FONT_SIZE_SMALL } from '../constants/theme';
 
 type TextProps = ComponentProps<typeof Text>;
 type ViewProps = ComponentProps<typeof View>;
@@ -11,18 +12,22 @@ type ViewProps = ComponentProps<typeof View>;
 type Props = ViewProps & {
   mode?: 'default' | 'with-title';
   title?: string;
+  subTitle?: string;
   children: ReactNode;
   titleContainerProps?: ViewProps;
   titleProps?: TextProps;
 };
 
 export default function Card(props: Props) {
-  let { mode, title, children, titleContainerProps, titleProps, ...otherProps } = props;
+  let { mode, title, subTitle, children, titleContainerProps, titleProps, ...otherProps } = props;
   return (
     <StyledCard {...otherProps}>
       {mode === 'with-title' && (
         <TitleContainer {...titleContainerProps}>
-          <Title {...titleProps}>{title}</Title>
+          <View flex>
+            <Title {...titleProps}>{title}</Title>
+            <SubTitle>{subTitle}</SubTitle>
+          </View>
         </TitleContainer>
       )}
       {children}
@@ -33,15 +38,20 @@ export default function Card(props: Props) {
 const StyledCard = styled(View)`
   border-radius: ${DEFAULT_BORDER_RADIUS};
   box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.1);
+  background-color: ${WHITE};
   overflow: hidden;
   background-color: ${WHITE};
 `;
 
 const TitleContainer = styled(View)`
+  flex-direction: row;
   background-color: ${THEME_COLOR};
   padding: 12px;
 `;
-
 const Title = styled(Text)`
   color: ${WHITE};
+`;
+const SubTitle = styled(Text)`
+  color: ${WHITE};
+  font-size: ${FONT_SIZE_SMALL};
 `;
