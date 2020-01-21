cac7f5b0b53849ca718eb56652732f88b1b41148
Nenye <57047007+nenye-a@users.noreply.github.com>
2020-01-21 12:31:49 -0800

    implented nearby method

--- data_insights_testing/data_aggregator/goog.py
+++ data_insights_testing/data_aggregator/goog.py
@@ -1,6 +1,8 @@
 from decouple import config
+import utils
 import requests
 import json
+import time
 
 '''
 
@@ -12,6 +14,7 @@ GOOG_KEY = config("GOOG_KEY")
 
 # Google endpoints. Refer to https://developers.google.com/places/web-service/intro for details.
 GOOG_FINDPLACE_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
+GOOG_NEARBY_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 GOOG_DETAILS_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/details/json'
 
 
@@ -33,32 +36,64 @@ def find(address, name="", bias='ipbias'):
     }
 
     # TODO: replace with some smart search when necessary
-    response = requests.request("GET", url, headers=headers, data=payload, params=params)
+    response = requests.request(
+        "GET", url, headers=headers, data=payload, params=params)
+
+    # TODO: store requested location in mongo
     return json.loads(response.text)
 
-def nearby():
 
-    return "INCOMPLETE"
+# Given google place ID, can generate the other nearby locations.
+# By defauly, algorithm returns all results in 3 miles up to 60.
+def nearby(lat, lng, category, radius=3, pagetoken=None):
+
+    url = GOOG_NEARBY_ENDPOINT
+
+    payload = {}
+    headers = {}
+
+    params = {
+        'key': GOOG_KEY
+    }
+
+    # get next page from previous request, or initiate new request
+    if pagetoken:
+        params.update({
+            'pagetoken': pagetoken
+        })
+    else:
+        location = str(lat) + ',' + str(lng)
+        radius = str(utils.miles_to_meters(radius))
+        params.update({
+            'location': location,
+            'radius': radius,
+            'type': category,
+            'language': 'en',
+            'rankby': 'prominence'
+        })
+
+    response = requests.request(
+        "GET", url, headers=headers, data=payload, params=params)
+    result = json.loads(response.text)
+
+    # if new page request fails, try again until it completes
+    if pagetoken and result['status'] == 'INVALID_REQUEST':
+        next_page = nearby(lat, lng, category, pagetoken=pagetoken)
+        result["results"].extend(next_page["results"])
+
+    if "next_page_token" in result:
+        next_page_token = result["next_page_token"]
+        next_page = nearby(lat, lng, category, pagetoken=next_page_token)
+        result["results"].extend(next_page["results"])
+
+    return result
 
 
-def nearby_stores():
-    return "INCOMPLATE"
+if __name__ == "__main__":
 
+    def test_find():
+        print(find('Spitz 371 E 2nd Street'))
 
-def nearby_office():
-    return "INCOMPLETE"
-
-
-def nearby_university():
-    return "INCOMPLETE"
-
-
-def nearby_hospitals():
-    return "INCOMPLETE"
-
-
-def nearby_garages():
-    return "INCOMPLETE"
-
-# if __name__ == "__main__":
-#     print(find('Spitz 371 E 2nd Street'))
+    def test_nearby():
+        item = nearby(34.0482327, -118.239857, 'restaurant')
+        print(len(item['results']))
--- data_insights_testing/data_aggregator/utils.py
+++ data_insights_testing/data_aggregator/utils.py
@@ -7,9 +7,20 @@ efficient & powerful
 
 '''
 
+MILES_TO_METERS_FACTOR = 1609.34
+
+
 # provided two lat & lng tuples, function returns distance in miles:
 # geo = (lat, lng)
 def distance(geo1, geo2):
     # calculate if item is beyond a certain distance
     mile_distance = geopy.distance.distance(geo1, geo2).miles
     return mile_distance
+
+
+def meters_to_miles(meters):
+    return meters/MILES_TO_METERS_FACTOR
+
+
+def miles_to_meters(miles):
+    return miles*MILES_TO_METERS_FACTOR
