86dcbc4f43680c41ae914b20409a14b763d43343
Katherine Philip <atsuzaki@users.noreply.github.com>
2020-01-23 10:16:29 +0700

    Node backend (#90)
    * Initial files
    
    * Set up express
    
    * Set up prisma
    
    * Feedback prisma model
    
    * Set up GraphQL server
    
    * Suppress lints
    
    * Run prettier
    
    * Remove unnecessary stuffs in package.json
    
    * Change datamodel to use mongoDB's id
    
    * Add (minimal) readme

---
+++ insembleapp/backend-node/src/resolvers/feedback/feedbackQuery.ts
@@ -0,0 +1,19 @@
+import { Root, Context } from 'serverTypes';
+import { FeedbackWhereInput } from '../../generated/prisma-client';
+
+function feedbacks(
+    _: Root,
+    { where }: { where: FeedbackWhereInput },
+    { prisma }: Context,
+) {
+    return prisma.feedbacks({
+        where,
+    });
+}
+
+// TODO: prisma wants the mongodb _id, can we PK with our id?
+// function feedback(_: Root, {id}: {id: string}, {prisma}: Context) {
+//     return prisma.feedback({id});
+// }
+
+export { feedbacks };
