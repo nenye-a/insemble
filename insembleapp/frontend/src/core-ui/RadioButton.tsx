ec33c009ce926490d1a5d714f757e532fe7a1b0e
Simon <sstur@me.com>
2020-01-23 17:25:48 -0800

    Implement RadioGroup

--- insembleapp/frontend/src/core-ui/RadioButton.tsx
+++ insembleapp/frontend/src/core-ui/RadioButton.tsx
@@ -1,82 +1,111 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { ComponentProps, useState } from 'react';
+import styled, { css } from 'styled-components';
 import View from './View';
-import { THEME_COLOR, SECONDARY_COLOR } from '../constants/colors';
-import { FONT_FAMILY_NORMAL, FONT_SIZE_NORMAL } from '../constants/theme';
+import Text from './Text';
+import { THEME_COLOR, WHITE } from '../constants/colors';
 
-type Props = {
-  option: string;
-  name: string; //we need the name so we can mark them as a group
-  isActive: () => void;
+type ViewProps = ComponentProps<typeof View>;
+
+type Props = ViewProps & {
+  name: string;
+  title: string;
+  isSelected: boolean;
+  onPress: () => void;
 };
 
+type RadioContainerProps = ViewProps & {
+  isFocused: boolean;
+};
+
+type StyledRadioProps = ViewProps & {
+  isVisible: boolean;
+};
+
+const SIZE = 18;
+const BORDER_RADIUS = SIZE / 2;
+
+const fillContainer = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+`;
+
+const focusedStyles = css`
+  box-shadow: 0 0 0.35rem rgba(0, 0, 0, 0.15);
+`;
+
+const RadioContainer = styled(View)<RadioContainerProps>`
+  width: ${SIZE}px;
+  height: ${SIZE}px;
+  border-radius: ${BORDER_RADIUS}px;
+  ${(props) => (props.isFocused ? focusedStyles : undefined)}
+`;
+
+const Backdrop = styled(View)`
+  ${fillContainer};
+  background-color: ${WHITE};
+  border: 1px solid ${THEME_COLOR};
+  border-radius: ${BORDER_RADIUS}px;
+`;
+
+const StyledRadio = styled(View)<StyledRadioProps>`
+  ${fillContainer};
+  background-color: ${WHITE};
+  background-clip: padding-box;
+  border: 5px solid ${THEME_COLOR};
+  border-radius: ${BORDER_RADIUS}px;
+  opacity: ${(props) => (props.isVisible ? 1 : 0)};
+  transition: opacity 150ms linear;
+`;
+
+const NativeRadio = styled.input.attrs(() => ({ type: 'radio' }))`
+  margin: 0;
+  border: 0;
+  padding: 0;
+  display: block;
+  box-sizing: border-box;
+  opacity: 0;
+  cursor: pointer;
+  ${fillContainer};
+`;
+
+const Row = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const TextLabel = styled(Text)`
+  padding-left: 6px;
+  /* Adding padding-top to make it look more centered */
+  padding-top: 1px;
+  cursor: pointer;
+`;
+
 export default function RadioButton(props: Props) {
-  let { option, isActive, name } = props;
+  let { name, id, title, isSelected, onPress, ...otherProps } = props;
+  let [isFocused, setFocus] = useState(false);
   return (
-    <RadioContainer>
-      {option}
-      <Radio value={option} name={name} onChange={isActive} />
-      <CheckMark />
-    </RadioContainer>
+    <Row {...otherProps}>
+      <RadioContainer isFocused={isFocused}>
+        <Backdrop />
+        <StyledRadio isVisible={isSelected} />
+        <NativeRadio
+          id={id}
+          name={name}
+          checked={isSelected}
+          onClick={onPress}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+        />
+      </RadioContainer>
+      <TextLabel as="label" htmlFor={id}>
+        {title}
+      </TextLabel>
+    </Row>
   );
 }
-
-const RadioContainer = styled.label`
-  display: block;
-  position: relative;
-  padding-left: 30px;
-  margin: 0 0 0 0;
-  font-family:${FONT_FAMILY_NORMAL}
-  font-size:${FONT_SIZE_NORMAL}
-  cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-`;
-
-const Radio = styled.input.attrs(() => ({ type: 'radio' }))`
-  ${RadioContainer} & {
-    position: absolute;
-    opacity: 0;
-    cursor: pointer;
-    height: 0;
-    width: 0;
-  }
-`;
-
-const CheckMark = styled(View)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 18px;
-  width: 18px;
-  background-color: white;
-  border-radius: 50%;
-  color: ${SECONDARY_COLOR};
-  border: solid;
-  border-width: 1px;
-  &: focus {
-    box-shadow: 0 0 0.35rem rgba(0, 0, 0, 0.15);
-  }
-  &: after {
-    content: '';
-    position: absolute;
-    display: none;
-    background: white;
-  }
-  ${RadioContainer} ${Radio}:checked ~& {
-    background-color: ${THEME_COLOR};
-  }
-  ${RadioContainer} ${Radio}:checked ~ &:after {
-    display: block;
-  }
-  ${RadioContainer} &:after {
-    top: 4px;
-    left: 4px;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background: white;
-  }
-`;
