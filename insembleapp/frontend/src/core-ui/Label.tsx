bf2fcdfada3d0b77fbea41bef27d499191205fcb
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-01-24 14:42:53 +0700

    move types

--- insembleapp/frontend/src/core-ui/Label.tsx
+++ insembleapp/frontend/src/core-ui/Label.tsx
@@ -4,14 +4,14 @@ import Text from './Text';
 import { THEME_COLOR } from '../constants/colors';
 import { FONT_SIZE_SMALL } from '../constants/theme';
 
-type Props = {
+type Props = TextProps & {
   text: string;
   id?: string;
 };
 
-export default function Label({ text, id }: Props) {
+export default function Label({ text, id, ...otherProps }: Props) {
   return (
-    <Text as="label" fontSize={FONT_SIZE_SMALL} color={THEME_COLOR} htmlFor={id}>
+    <Text as="label" fontSize={FONT_SIZE_SMALL} color={THEME_COLOR} htmlFor={id} {...otherProps}>
       {text}
     </Text>
   );
