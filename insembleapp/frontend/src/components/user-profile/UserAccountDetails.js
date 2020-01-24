a69c029faedc34892ed5b83c4198842349666b3a
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-01-24 12:44:08 +0700

    change component name

--- insembleapp/frontend/src/components/user-profile/UserAccountDetails.js
+++ insembleapp/frontend/src/components/user-profile/UserAccountDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardHeader, ListGroup, ListGroupItem, Row, Col, Form, Button } from 'shards-react';
-import { TextInputWithLabel as TextInput } from '../../core-ui/index';
+import { TextInput } from '../../core-ui/index';
 
 export default function UserAccountDetails({ title }) {
   return (
