ff358aa668e6a536161957302e127857ed0c3885
Katherine Philip <kurerunoshikaku@yahoo.com>
2020-01-24 13:56:05 +0700

    Wrapper for categories + a bunch of other stuff

---
+++ insembleapp/backend-node/src/resolvers/category/categoryQuery.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { Root, Context } from 'serverTypes';
+import { LEGACY_API_URI } from '../../constants/host';
+
+export interface CategoryWhereInput {
+    category?: string | null;
+    category_contains?: string | null;
+}
+
+type CategoryArgs = {
+    where?: CategoryWhereInput,
+    skip?: number,
+    first?: number,
+    last?: number,
+}
+
+// TODO: fetching categories can be cached
+async function categories(
+    _: Root,
+    {where, skip, first, last} : CategoryArgs,
+    _context: Context,
+) {
+    let url = `${LEGACY_API_URI}/api/category`;
+    let categories: Array<string> = (await axios.get(url)).data;
+
+    if (first && last) {
+        return { errors: ["Error"]}; // TODO: how to properly error?
+    }
+
+    // Process where input
+    if (where) {
+        let {category, category_contains: categoryContains} = where;
+
+        if (typeof category === 'string') {
+            categories = categories.filter((x) => x === category);
+        }
+        if (typeof categoryContains === 'string') {
+            categories = categories.filter((x) => x.includes(categoryContains));
+        }
+    }
+
+    // Process pagination
+    if (skip) {
+        categories = categories.slice(skip, categories.length);
+    }
+    if (first) {
+        categories = categories.slice(0, first);
+    }
+    if (last) {
+        categories = categories.slice(categories.length - last, categories.length);
+    }
+
+    return categories;
+}
+
+export { categories };
--- insembleapp/backend-node/src/resolvers/index.ts
+++ insembleapp/backend-node/src/resolvers/index.ts
@@ -1,7 +1,11 @@
-import { feedbacks } from './feedback/feedbackQuery';
+import { categories } from './category/categoryQuery';
+import { feedback, feedbacks } from './feedback/feedbackQuery';
 
 export default {
     Query: {
+        categories,
+
+        feedback,
         feedbacks,
     },
 };
