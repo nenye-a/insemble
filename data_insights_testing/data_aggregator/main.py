8102b96d2aea78b08e48e6481cd7887068de6f9b
Chukwunenye Anagbogu <chukwunenyeanagbogu@ChukwunnyesiMac.attlocal.net>
2020-01-26 18:52:07 -0800

    run_spatial creation + main

--- data_insights_testing/data_aggregator/main.py
+++ data_insights_testing/data_aggregator/main.py
@@ -315,7 +315,11 @@ def place_collector_google(start_lat, start_lng, type_, run_name, bounds, run_re
 
 ###############################################
 
-
+# This place collector will preform a google_nearby in brute force on a city to obtain all locations, within a bounded box.
+# The collector, will find the distance and will continue to find distance on items until it spans the region.
+# This function operates similarly to the previous Google collector, but will instead "walk" over a city in order to obtain all the
+# details deemed necessary, instead of searching in a BFS fashion. Bounds are provided similar to how they are above,
+# in the form of {'nw':(lat1,lng1), 'se'(lat2,lng2)}. Bounds only function for North America
 def place_collector_google_brute(start_lat, start_lng, type_, run_name, bounds, run_record=None, direction='down'):
 
     horizontal_bearing = 90 if direction == 'down' else 270
@@ -350,7 +354,7 @@ def place_collector_google_brute(start_lat, start_lng, type_, run_name, bounds, 
 
     smallest_distance_observed = 1.5
     first_horizontal_call = None
-    
+
     # start where we left off if this record existed
     if len(run_record['calls']) > 0:
         start_lat = run_record['calls'][0]['next_lat']
@@ -411,7 +415,7 @@ def place_collector_google_brute(start_lat, start_lng, type_, run_name, bounds, 
             print(err)
             print("(CC_GOOGLE) **** Attempted to insert {} into DB.".format(len(places)))
             print("(CC_GOOGLE) **** Failed to input all, likely due to duplicates")
-        
+
         # calculate the distance of coverage for this search
         furthest_distance = utils.distance(
             (start_lat, start_lng),
@@ -471,7 +475,8 @@ def place_collector_google_brute(start_lat, start_lng, type_, run_name, bounds, 
         DB_COLLECT.update_one({'run_name': run_name}, {'$set': run_record})
 
         start_lat, start_lng = next_lat, next_lng
-        print("\n(CC_GOOGLE) ****** Status update | name: {}".format(run_record['run_name']))
+        print(
+            "\n(CC_GOOGLE) ****** Status update | name: {}".format(run_record['run_name']))
         print("\n(CC_GOOGLE) ****** Moving to the next point after the following settings:\n{}\n ".format(call_update))
 
 
@@ -591,7 +596,7 @@ def detail_builder():
             update_count += 1
             if update_count % update_size == 0:
                 print(
-                    "(DD) ****** DETAILER: {} more places detailed".format(update_count/update_size))
+                    "(DD) ****** DETAILER: {} more places detailed".format(update_size))
                 print(
                     "(DD) ****** Total documents detailed in this run: {}".format(update_count))
 
@@ -714,17 +719,27 @@ def psycho_builder(radius=1):
             # update with spatial data
             lat = space['geometry']['location']['lat']
             lng = space['geometry']['location']['lng']
-            psycho_dict = spatial.get_psychographics(
-                lat, lng, radius, spatial_df, block_df, cats)
+            psycho_dict1 = spatial.get_psychographics(
+                lat, lng, 1, spatial_df, block_df, cats)
+            psycho_dict3 = spatial.get_psychographics(
+                lat, lng, 3, spatial_df, block_df, cats)
+            psycho_dict5 = spatial.get_psychographics(
+                lat, lng, 5, spatial_df, block_df, cats)
 
             # space has been detailed and will be updated
             DB_PROCESSED_SPACE.update_one(
-                {'place_id': place_id}, {'$set': {'psycho': psycho_dict, 'psycho_finished': True}})
+                {'place_id': place_id}, {'$set': {
+                    'psycho1': psycho_dict1,
+                    'psycho3': psycho_dict3,
+                    'psycho5': psycho_dict5,
+                    'psycho_finished': True
+                }
+                })
 
             update_count += 1
             if update_count % update_size == 0:
                 print(
-                    "(DD) ****** PSYCHO DETAILS: {} more places psycho detailed".format(update_count/update_size))
+                    "(DD) ****** PSYCHO DETAILS: {} more places updated with psychographics".format(update_size))
                 print(
                     "(DD) ****** Total documents psycho detailed in this run: {}".format(update_count))
 
