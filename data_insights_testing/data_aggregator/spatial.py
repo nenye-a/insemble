c5827afcbc18fe5a4ac284cd9d8810432e0e29a4
davidboxboro <davidboxboro@gmail.com>
2020-01-26 18:51:01 -0800

    spatial.py update

--- data_insights_testing/data_aggregator/spatial.py
+++ data_insights_testing/data_aggregator/spatial.py
@@ -6,18 +6,12 @@ File for spatial analytics data processing
 
 from utils import distance 
 import pandas as pd
-
-
-def weight(arr, const):
-    for i in arr:
-        i *= const
+import time
 
 # get psychographics given lat and lng 
-def get_psychographics(lat, lng, radius, spatial_df, block_grp_df):
-
+def get_psychographics(lat, lng, radius, spatial_df, block_grp_df, cats):
     # get block groups overlapping w circle 
     block_grp_df["dist"] = block_grp_df.apply(lambda row : distance((row["lat"], row["lng"]), (lat, lng)), axis = 1)
-
     # isolate block groups that we want
     df_trimmed = block_grp_df[block_grp_df["dist"] <= radius]
 
@@ -30,11 +24,12 @@ def get_psychographics(lat, lng, radius, spatial_df, block_grp_df):
 
     # compute result
     sums = list(spatial_trimmed.sum(axis=1))
-    return [sums[i]/sums[-1] for i in range(0, len(sums)-1)]
+    data = [sums[i]/sums[-1] for i in range(0, len(sums)-1)]
 
+    return dict(zip(cats, data))
 
 # create dataframe with all spatial data
-def create_spatial_df():
+def create_spatial_cats_and_df():
     spatial_dict = {}
     f = open("raw_data/Spatial_Los_Angeles_Oct1_2019.csv", "r")
     f = f.readlines()
@@ -43,12 +38,15 @@ def create_spatial_df():
         if line[1] == "":
             continue
         spatial_dict[int(line[0])] = [float(line[i])*float(line[-1]) for i in range(1, len(line)-1)] + [float(line[-1])]
+    
+    # get categories
+    cats = f[1].rstrip().split(",")[1:-1]
 
-    return pd.DataFrame(data=spatial_dict)
+    return cats, pd.DataFrame(data=spatial_dict)
 
 # create block group to lat, long dataframe
 def create_block_grp_df():
-    f = open("raw_data/cbg_geographic_data.csv", "r")
+    f = open("raw_data/cbg_geographic_data_LA.csv", "r")
     f = f.readlines()
     d = {"lat": [], "lng": [], "block_grp": []}
     for line in f[1:]:
@@ -59,6 +57,12 @@ def create_block_grp_df():
 
     return pd.DataFrame(data=d)
 
+
 if __name__ == "__main__":
-    ret=get_psychographics(34.0523, -118.2395, 0.5, create_spatial_df(), create_block_grp_df())
+    cats, spatial_df = create_spatial_cats_and_df()
+    block_df = create_block_grp_df()
+  
+    ret=get_psychographics(34.0523, -118.2395, 0.5, spatial_df, block_df, cats)
+    print(ret)
+    ret=get_psychographics(34.0430, -118.2673, 0.5, spatial_df, block_df, cats)
     print(ret)
