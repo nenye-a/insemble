ded86148974bac92715eabf7b61b9604a6cffae3
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-01-27 11:14:15 +0700

    alert core ui

---
+++ insembleapp/frontend/src/components/icons/info.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { IconProps } from '../../types/types';
+import { THEME_COLOR } from '../../constants/colors';
+
+const SvgInfo = ({ fill, ...otherProps }: IconProps) => (
+  <svg
+    id="info-24px_4_"
+    data-name="info-24px (4)"
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 18 18"
+    {...otherProps}
+  >
+    <path id="Path_3900" data-name="Path 3900" d="M0,0H18V18H0Z" fill="none" />
+    <path
+      id="Path_3901"
+      data-name="Path 3901"
+      d="M8.75,5.75h1.5v1.5H8.75Zm0,3h1.5v4.5H8.75ZM9.5,2A7.5,7.5,0,1,0,17,9.5,7.5,7.5,0,0,0,9.5,2Zm0,13.5a6,6,0,1,1,6-6A6.008,6.008,0,0,1,9.5,15.5Z"
+      transform="translate(-0.5 -0.5)"
+      fill={fill || THEME_COLOR}
+    />
+  </svg>
+);
+
+export default SvgInfo;
