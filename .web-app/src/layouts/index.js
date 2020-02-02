82e2add051c6cee52db3ee2db48cd636da69cf9b
Simon Sturmer <sstur@me.com>
2020-02-02 18:03:31 -0500

    Move project out of subdirectory into root (#127)
    * Move insembleapp/* -> root
    
    * CircleCI: disable failing backend tests

--- .web-app/src/layouts/index.js
+++
@@ -1,7 +0,0 @@
-import DefaultLayout from "./Default";
-import TenantSpaces from "./TenantSpaces";
-import HeaderNavigation from "./HeaderNavigation";
-import IconSidebar from "./IconSidebar";
-import LandlordLayout from "./Landlord";
-
-export { DefaultLayout, HeaderNavigation, IconSidebar, LandlordLayout, TenantSpaces };
