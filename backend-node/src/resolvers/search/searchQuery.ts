47e750b7510f865141a54d56442cf9b10e386781
Katherine Philip <kurerunoshikaku@yahoo.com>
2020-02-05 13:42:03 +0700

    Change age range input to an error-prone shape

--- backend-node/src/resolvers/search/searchQuery.ts
+++ backend-node/src/resolvers/search/searchQuery.ts
@@ -6,13 +6,25 @@ import { LEGACY_API_URI } from '../../constants/host';
 type SearchInput = {
     categories: Array<string>,
     target_income: number,
-    target_age?: [number, number],
+    target_age?: {
+        min: number,
+        max: number,
+    },
     target_psychographics?: Array<string>,
     // property_criteria: object, // TODO: type. gql needs it
 }
 
 async function search(_: Root, args : SearchInput, _ctx: Context) {
-    let response = (await axios.post(`${LEGACY_API_URI}/api/search/`, args));
+    // Convert target_age from object format to [min, max] array format the backend expects
+    let data;
+    if (args.target_age != null) {
+        let tAge = args.target_age;
+        data = { ...args, target_age: [tAge.min, tAge.max]};
+    } else {
+        data = args;
+    }
+
+    let response = (await axios.post(`${LEGACY_API_URI}/api/search/`, data));
 
     return {
         // eslint-disable-next-line no-underscore-dangle
