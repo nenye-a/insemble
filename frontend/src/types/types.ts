501bec61a61723db2b9567ad5e98004e39f64198
Filberts <filberts@Filbert-KodeFox-2.local>
2020-02-05 16:32:45 +0700

    Add Demographics Card

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,6 +1,8 @@
-import { CSSProperties } from 'react';
+import React, { CSSProperties } from 'react';
 
 export type IconProps = {
   fill?: string;
   style?: CSSProperties;
+  x?: string | number;
+  y?: string | number;
 };
