b4accd98751ba0ef4b804f442f3b32696cfc1a8b
Katherine Philip <atsuzaki@users.noreply.github.com>
2020-02-07 00:03:46 +0700

    Run prettier (#137)

--- backend-node/src/resolvers/category/categoryQuery.ts
+++ backend-node/src/resolvers/category/categoryQuery.ts
@@ -8,27 +8,31 @@ export interface CategoryWhereInput {
 }
 
 type CategoryArgs = {
-    where?: CategoryWhereInput,
-    skip?: number,
-    first?: number,
-    last?: number,
-}
+    where?: CategoryWhereInput;
+    skip?: number;
+    first?: number;
+    last?: number;
+};
 
 // TODO: fetching categories can be cached
 async function categories(
     _: Root,
-    {where, skip, first, last} : CategoryArgs,
+    { where, skip, first, last }: CategoryArgs,
     _context: Context,
 ) {
     if (first && last) {
-        throw new Error("Including a value for both first and last is not supported.");
+        throw new Error(
+            'Including a value for both first and last is not supported.',
+        );
     }
 
-    let categories: Array<string> = (await axios.get(`${LEGACY_API_URI}/api/category`)).data;
+    let categories: Array<string> = (
+        await axios.get(`${LEGACY_API_URI}/api/category`)
+    ).data;
 
     // Process where input
     if (where) {
-        let {category, category_contains: categoryContains} = where;
+        let { category, category_contains: categoryContains } = where;
 
         if (typeof category === 'string') {
             categories = categories.filter((x) => x === category);
@@ -46,7 +50,10 @@ async function categories(
         categories = categories.slice(0, first);
     }
     if (last) {
-        categories = categories.slice(categories.length - last, categories.length);
+        categories = categories.slice(
+            categories.length - last,
+            categories.length,
+        );
     }
 
     return categories;
