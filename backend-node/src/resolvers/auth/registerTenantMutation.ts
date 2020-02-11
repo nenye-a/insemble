cdd98f13ca7e80d048845506870a02d0a8b9fbfc
Joshua Setiawan <joshuasetiawan04@gmail.com>
2020-02-11 18:12:57 +0700

    Add Register tenant resolver (#147)
    * add register tenant schema
    
    * add registerTenantResolver

---
+++ backend-node/src/resolvers/auth/registerTenantMutation.ts
@@ -0,0 +1,18 @@
+import { Root, Context } from 'serverTypes';
+
+async function registerTenant(_: Root, __: any, ___: Context) {
+    return {
+        token: 'put token here',
+        tenant: {
+            id: '1',
+            email: 'DELETED_EMAIL',
+            firstName: 'firstname',
+            lastName: 'lastname',
+            avatar: '',
+            company: 'mock company',
+            tier: 'FREE',
+        },
+    };
+}
+
+export { registerTenant };
