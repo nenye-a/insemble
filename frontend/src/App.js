f9d6bbd8e3734084986b3a4fc7ca444a4f1b7a3d
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-02-12 17:25:39 +0700

    apply suggestions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { transitions, positions, Provider as AlertProvider } from 'react-alert';
 import { ClientContextProvider } from 'react-fetching-library';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import client from './client';
 import apolloClient from './graphql/apolloClient';
+import store from './redux/store';
+import { loadUser } from './redux/actions/auth';
+import routes from './routes';
+import withTracker from './withTracker';
 
 // optional configuration
 const options = {
@@ -18,16 +23,9 @@ const options = {
   transition: transitions.SCALE,
 };
 
-import routes from './routes';
-import withTracker from './withTracker';
-
 const trackingID = 'UA-153536736-1';
 import ReactGA from 'react-ga';
 ReactGA.initialize(trackingID);
-
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { loadUser } from './redux/actions/auth';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/main.scss';
@@ -51,10 +49,10 @@ class App extends React.Component {
       <ApolloProvider client={apolloClient}>
         <ClientContextProvider client={client}>
           <AlertProvider template={MyTemplate} {...options}>
-            <Provider store={store}>
+            <ReduxProvider store={store}>
               <Router basename={process.env.REACT_APP_BASENAME || ''}>
                 <Switch>
-                  <div>
+                  <>
                     {routes.map((route, index) => {
                       return (
                         <Route
@@ -71,10 +69,10 @@ class App extends React.Component {
                         />
                       );
                     })}
-                  </div>
+                  </>
                 </Switch>
               </Router>
-            </Provider>
+            </ReduxProvider>
           </AlertProvider>
         </ClientContextProvider>
       </ApolloProvider>
--- frontend/src/graphql/queries/categories.ts
+++
@@ -1,8 +0,0 @@
-import gql from 'graphql-tag';
-
-export const GET_CATEGORY = gql`
-  query getCategory {
-    categoryData(input: $input)
-      @rest(type: "GetCategoryDataQuery", method: "GET", path: "/category")
-  }
-`;
