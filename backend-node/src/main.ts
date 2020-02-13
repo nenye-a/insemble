fc5fe45a43fef3cc1c1bd9f8b838bd7c56a5632f
Joshua Setiawan <joshuasetiawan04@gmail.com>
2020-02-13 12:01:21 +0700

    DataModeling and Prisma2 upgrade (#155)
    * add tabwidh  prettier
    
    * add packages and packages requirement
    
    * remove old prisma 1 settings
    
    * add docker db file
    
    * add schema.prisma
    
    * remove schema.graphql
    
    * add initial migrations
    
    * move to prisma2
    
    * remove feedback query
    
    * move schema to prisma2

--- backend-node/src/main.ts
+++ backend-node/src/main.ts
@@ -1,15 +1,20 @@
 import { GraphQLServer } from 'graphql-yoga';
-import resolvers from './resolvers';
-import { prisma } from './generated/prisma-client/';
+import { PrismaClient } from '@prisma/client';
+
+import { schema } from '../src/schema';
+import { Context } from 'serverTypes';
+
+const prisma = new PrismaClient();
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-    context: {
-        prisma,
-    },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-} as any);
+  schema,
+  context: (): Context => {
+    return {
+      prisma,
+    };
+  },
+});
 
-// eslint-disable-next-line no-console
-server.start(() => console.log('Server is running on http://localhost:4000'));
+server.start(() => {
+  console.log(`Server is running on http://localhost:4000`);
+});
