f978595ac49774b6ccf3f988761cce72e788e514
RaymondAnggara <raymond@kodefox.com>
2020-02-13 15:56:49 +0700

    Mock Login API after Prisma2

--- backend-node/src/resolvers/auth/loginMutation.ts
+++ backend-node/src/resolvers/auth/loginMutation.ts
@@ -1,26 +1,32 @@
 import { Root, Context } from 'serverTypes';
+import { mutationField, stringArg } from 'nexus';
 
-async function loginTenant(
-    _: Root,
-    { email, password }: { email: string; password: string },
-    _context: Context,
+let loginTenant = mutationField('loginTenant', {
+  type: 'LoginTenant',
+  args: {
+    email: stringArg({ required: true }),
+    password: stringArg({ required: true }),
+  },
+  resolve: loginTenantResolver,
+});
+
+async function loginTenantResolver(
+  _: Root,
+  { email, password }: { email: string; password: string },
+  _context: Context,
 ) {
-    if (password) {
-        return {
-            tenant: {
-                id: '1',
-                email,
-                firstName: 'Sams',
-                lastName: 'Udin',
-                company: 'Mock Fried Chicken',
-                tier: 'Free',
-                role: 'Tenant',
-                avatar: '',
-            },
-            token: 's3cr3t-t0ken_here',
-        };
-    }
-    return null;
+  return {
+    tenant: {
+      id: '1',
+      email,
+      firstName: 'Sams',
+      lastName: 'Udin',
+      company: 'Mock Fried Chicken',
+      tier: 'Free',
+      avatar: '',
+    },
+    token: 's3cr3t-t0ken_here',
+  };
 }
 
 export { loginTenant };
