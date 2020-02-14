a48bfb478806ecb139315d0576144a71144f2e4e
Joshua <joshuasetiawan04@gmail.com>
2020-02-14 16:27:19 +0700

    export prisma client

--- backend-node/src/main.ts
+++ backend-node/src/main.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 import { schema } from '../src/schema';
 import { Context } from 'serverTypes';
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 const server = new GraphQLServer({
   schema,
