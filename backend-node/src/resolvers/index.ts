f916f62006e83d64e1c07710d93cb6dde4aaf6f9
RaymondAnggara <raymond@kodefox.com>
2020-02-14 13:37:20 +0700

    Add educationQuery filter

--- backend-node/src/resolvers/index.ts
+++ backend-node/src/resolvers/index.ts
@@ -1,2 +1,6 @@
 export * from './auth/registerTenantMutation';
 export * from './auth/loginMutation';
+export * from './filter/categoryQuery';
+export * from './filter/personaQuery';
+export * from './filter/commuteQuery';
+export * from './filter/educationQuery';
