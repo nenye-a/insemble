6236d0449c1c2b38b8cd9371247144c88f5e4136
Joshua <joshuasetiawan04@gmail.com>
2020-02-18 15:29:11 +0700

    add brandThumbnail type

---
+++ backend-node/src/typeSchemas/BrandThumbnail.ts
@@ -0,0 +1,15 @@
+import { objectType } from 'nexus';
+
+export let BrandThumbnail = objectType({
+  name: 'BrandThumbnail',
+  definition(t) {
+    t.model('Brand').id;
+    t.model('Brand').name();
+    t.model('Brand').categories();
+    t.field('matchingLocations', {
+      type: 'MatchingLocation',
+      list: true,
+      nullable: true,
+    });
+  },
+});
