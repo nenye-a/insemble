5ad91c09594284f5e2244f4036a5b0a0e5d0ceeb
Joshua Setiawan <joshuasetiawan04@gmail.com>
2020-02-19 15:31:53 +0700

    Tenant edit profile (#183)
    * add phoneNumber to tenant model
    
    * comment out permission middleware import
    
    * add edit profile input and resolver

---
+++ backend-node/src/typeSchemas/EditProfileInput.ts
@@ -0,0 +1,16 @@
+import { inputObjectType } from 'nexus';
+
+export let EditProfileInput = inputObjectType({
+  name: 'EditProfileInput',
+  definition(t) {
+    t.string('email');
+    t.string('firstName');
+    t.string('lastName');
+    t.string('company');
+    t.string('title');
+    t.string('phoneNumber');
+    t.string('description');
+    t.string('oldPassword');
+    t.string('newPassword');
+  },
+});
