b75b6384948a6a770d103de0487d7c240374ae5c
Joshua Setiawan <joshuasetiawan04@gmail.com>
2020-02-19 17:51:35 +0700

    Edit brand (#185)
    * add editbrand mutation
    
    * refactor tenant matches

--- backend-node/src/typeSchemas/BrandThumbnail.ts
+++ backend-node/src/typeSchemas/BrandThumbnail.ts
@@ -3,7 +3,7 @@ import { objectType } from 'nexus';
 export let BrandThumbnail = objectType({
   name: 'BrandThumbnail',
   definition(t) {
-    t.model('Brand').id;
+    t.model('Brand').id();
     t.model('Brand').name();
     t.model('Brand').categories();
     t.field('matchingLocations', {
