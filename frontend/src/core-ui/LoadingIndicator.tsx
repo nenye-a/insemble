872e5f2ed8e60879cb9cb43c83306296ffb40fa0
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-02-19 11:55:23 +0700

    Various Fixes (#179)
    * add loading indicator to overlay
    
    * prefill categories
    
    * validate input before signing up

--- frontend/src/core-ui/LoadingIndicator.tsx
+++ frontend/src/core-ui/LoadingIndicator.tsx
@@ -3,22 +3,33 @@ import styled from 'styled-components';
 import loadingWhite from '../assets/images/loading-white.gif';
 import loadingPurple from '../assets/images/loading-purple.gif';
 
+type IconSize = keyof typeof ICON_SIZES;
 type Props = {
   color: 'purple' | 'white';
   visible?: boolean;
+  size?: IconSize;
+};
+
+type IconProps = {
+  size: string;
 };
 
 export default function LoadingIndicator(props: Props) {
-  let { color, visible = true } = props;
+  let { color, visible = true, size = 'small' as IconSize } = props;
 
   if (visible) {
-    return <Icon src={color === 'white' ? loadingWhite : loadingPurple} />;
+    return <Icon src={color === 'white' ? loadingWhite : loadingPurple} size={ICON_SIZES[size]} />;
   }
   return null;
 }
 
-const Icon = styled.img`
+const ICON_SIZES = {
+  small: '25px',
+  large: '100px',
+};
+
+const Icon = styled.img<IconProps>`
   object-fit: contain;
-  width: 25px;
-  height: 25px;
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
 `;
