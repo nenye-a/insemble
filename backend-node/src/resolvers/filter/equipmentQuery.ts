900938f53b5c8319473c769f8261bfcd08da543d
RaymondAnggara <raymond@kodefox.com>
2020-02-20 15:02:34 +0700

    Update equipment filter

--- backend-node/src/resolvers/filter/equipmentQuery.ts
+++ backend-node/src/resolvers/filter/equipmentQuery.ts
@@ -1,24 +1,33 @@
+import axios from 'axios';
 import { queryField, FieldResolver } from 'nexus';
 
 import { Root, Context } from 'serverTypes';
+import { LEGACY_API_URI } from '../../constants/host';
+import { FilterOptions } from 'dataTypes';
 
 let equipmentsResolver: FieldResolver<'Query', 'equipments'> = async (
   _: Root,
   _args,
   _context: Context,
 ) => {
-  return [
-    'chair',
-    'table',
-    'desk',
-    'clock',
-    'lamp',
-    'cabinet',
-    'fridge',
-    'heater',
-    'curtains',
-    'couch',
-  ];
+  let { equipment }: FilterOptions = (
+    await axios.get(`${LEGACY_API_URI}/api/filter/`)
+  ).data;
+
+  return equipment
+    ? equipment
+    : [
+        'chair',
+        'table',
+        'desk',
+        'clock',
+        'lamp',
+        'cabinet',
+        'fridge',
+        'heater',
+        'curtains',
+        'couch',
+      ];
 };
 
 let equipments = queryField('equipments', {
