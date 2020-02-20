5eaa95e139874086a96ee987b53596f1b097a00e
RaymondAnggara <raymond@kodefox.com>
2020-02-20 15:03:43 +0700

    Add ethnicity filter

---
+++ backend-node/src/resolvers/filter/ethnicityQuery.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { Root, Context } from 'serverTypes';
+import { LEGACY_API_URI } from '../../constants/host';
+import { queryField } from 'nexus';
+import { FilterOptions } from 'dataTypes';
+
+let ethnicity = queryField('ethnicity', {
+  type: 'String',
+  list: true,
+  resolve: ethnicityResolver,
+});
+
+// TODO: Add where args
+async function ethnicityResolver(_: Root, _args: {}, _context: Context) {
+  let { ethnicity }: FilterOptions = (
+    await axios.get(`${LEGACY_API_URI}/api/filter/`)
+  ).data;
+
+  return ethnicity ? ethnicity : [];
+}
+
+export { ethnicity };
