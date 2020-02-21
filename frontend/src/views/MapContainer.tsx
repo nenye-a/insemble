c7a18fcc50b1cc2640a1322d4b638633ed497445
Nenye <57047007+nenye-a@users.noreply.github.com>
2020-02-21 10:58:20 -0800

    change map syle (#198)

--- frontend/src/views/MapContainer.tsx
+++ frontend/src/views/MapContainer.tsx
@@ -209,6 +209,317 @@ function MapContainer({ onMarkerClick, matchingLocations }: Props) {
           maxZoom: 15,
           minZoom: 7,
           mapTypeControl: false,
+          styles: [
+            {
+              elementType: 'geometry',
+              stylers: [
+                {
+                  color: '#f5f5f5',
+                },
+              ],
+            },
+            {
+              elementType: 'labels',
+              stylers: [
+                {
+                  visibility: 'off',
+                },
+              ],
+            },
+            {
+              elementType: 'labels.icon',
+              stylers: [
+                {
+                  visibility: 'off',
+                },
+              ],
+            },
+            {
+              elementType: 'labels.text.fill',
+              stylers: [
+                {
+                  color: '#616161',
+                },
+              ],
+            },
+            {
+              elementType: 'labels.text.stroke',
+              stylers: [
+                {
+                  color: '#f5f5f5',
+                },
+              ],
+            },
+            {
+              featureType: 'administrative',
+              elementType: 'labels',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+              ],
+            },
+            {
+              featureType: 'administrative.land_parcel',
+              elementType: 'labels.text.fill',
+              stylers: [
+                {
+                  color: '#bdbdbd',
+                },
+              ],
+            },
+            {
+              featureType: 'administrative.neighborhood',
+              stylers: [
+                {
+                  visibility: 'off',
+                },
+              ],
+            },
+            {
+              featureType: 'landscape.natural',
+              stylers: [
+                {
+                  color: '#ccf6cb',
+                },
+              ],
+            },
+            {
+              featureType: 'poi',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+              ],
+            },
+            {
+              featureType: 'poi',
+              elementType: 'geometry',
+              stylers: [
+                {
+                  color: '#eeeeee',
+                },
+              ],
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+              ],
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [
+                {
+                  color: '#757575',
+                },
+              ],
+            },
+            {
+              featureType: 'poi.business',
+              stylers: [
+                {
+                  visibility: 'simplified',
+                },
+              ],
+            },
+            {
+              featureType: 'poi.business',
+              elementType: 'labels.text',
+              stylers: [
+                {
+                  color: '#a9a9a9',
+                },
+              ],
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [
+                {
+                  color: '#c4f9cd',
+                },
+              ],
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text',
+              stylers: [
+                {
+                  visibility: 'off',
+                },
+              ],
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [
+                {
+                  color: '#9e9e9e',
+                },
+              ],
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [
+                {
+                  color: '#ffffff',
+                },
+              ],
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+                {
+                  weight: 1.5,
+                },
+              ],
+            },
+            {
+              featureType: 'road.arterial',
+              elementType: 'labels.text.fill',
+              stylers: [
+                {
+                  color: '#757575',
+                },
+              ],
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [
+                {
+                  color: '#000000',
+                },
+                {
+                  lightness: 80,
+                },
+                {
+                  weight: 1,
+                },
+              ],
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels',
+              stylers: [
+                {
+                  visibility: 'simplified',
+                },
+              ],
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [
+                {
+                  color: '#616161',
+                },
+              ],
+            },
+            {
+              featureType: 'road.local',
+              elementType: 'labels.text.fill',
+              stylers: [
+                {
+                  color: '#9e9e9e',
+                },
+              ],
+            },
+            {
+              featureType: 'transit.line',
+              elementType: 'geometry',
+              stylers: [
+                {
+                  color: '#f0dee1',
+                },
+                {
+                  visibility: 'off',
+                },
+                {
+                  weight: 1,
+                },
+              ],
+            },
+            {
+              featureType: 'transit.line',
+              elementType: 'labels',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+              ],
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'geometry',
+              stylers: [
+                {
+                  color: '#eeeeee',
+                },
+              ],
+            },
+            {
+              featureType: 'transit.station.airport',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+              ],
+            },
+            {
+              featureType: 'transit.station.bus',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+              ],
+            },
+            {
+              featureType: 'transit.station.bus',
+              elementType: 'labels.icon',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+              ],
+            },
+            {
+              featureType: 'transit.station.rail',
+              stylers: [
+                {
+                  visibility: 'on',
+                },
+              ],
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [
+                {
+                  color: '#c9d5f1',
+                },
+              ],
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [
+                {
+                  color: '#9e9e9e',
+                },
+              ],
+            },
+          ],
         }}
         onClick={(event) => onMapClick(event.latLng)}
         onBoundsChanged={onBoundsChanged}
