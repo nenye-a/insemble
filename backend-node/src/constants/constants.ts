d166512f4780b423c79cba1efeca8e263a2a3eff
Joshua <joshuasetiawan04@gmail.com>
2020-02-23 15:13:19 +0700

    add host to env

--- backend-node/src/constants/constants.ts
+++ backend-node/src/constants/constants.ts
@@ -3,5 +3,6 @@ dotenv.config();
 
 let SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '';
 let NODE_ENV = process.env.NODE_ENV || 'development';
+let HOST = process.env.HOST || 'http://localhost:4000'; // NOTES: make sure to set HOST on the env for production
 
-export { SENDGRID_API_KEY, NODE_ENV };
+export { HOST, NODE_ENV, SENDGRID_API_KEY };
