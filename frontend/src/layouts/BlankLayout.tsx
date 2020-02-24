496ecb3456c5f531fe4842f825770e10454a4664
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-02-24 12:10:12 +0700

    remove unused files

--- frontend/src/layouts/BlankLayout.tsx
+++ frontend/src/layouts/BlankLayout.tsx
@@ -1,3 +1,3 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-export default ({ children }) => <>{children}</>;
+export default ({ children }: { children: ReactNode }) => <>{children}</>;
--- frontend/src/layouts/index.ts
+++ frontend/src/layouts/index.ts
@@ -1,8 +1,5 @@
-import TenantSpaces from './TenantSpaces';
-import Tenant from './Tenant';
-import LandlordLayout from './Landlord';
 import BlankLayout from './BlankLayout';
 import BasicLayout from './BasicLayout';
 import UserProfileLayout from './UserProfileLayout';
 
-export { LandlordLayout, TenantSpaces, Tenant, BlankLayout, BasicLayout, UserProfileLayout };
+export { BlankLayout, BasicLayout, UserProfileLayout };
