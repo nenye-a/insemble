53fe793f8bb842f5b7069b7e2882359b9bab0af1
Chukwunenye Anagbogu <nenye@insemblegroup.com>
2020-02-23 22:53:23 -0800

    error handling for run_block

--- data_insights_testing/data_aggregator/run_block.py
+++ data_insights_testing/data_aggregator/run_block.py
@@ -35,6 +35,10 @@ def blockify():
             lng = round(place['lng'], 6)
 
             block = anmspatial.point_to_block(lat, lng, state='CA', prune_leading_zero=False)
+
+            if not block:
+                continue
+
             blockgroup = block[:-3]
 
             update = {
