d75b1d55e245f739522ee7b12287516b8ee1d7d7
Chukwunenye Anagbogu <nenye@insemblegroup.com>
2020-02-23 21:00:25 -0800

    update anmspatial

--- data_insights_testing/data_aggregator/anmspatial.py
+++ data_insights_testing/data_aggregator/anmspatial.py
@@ -64,9 +64,55 @@ def point_to_block_group(lat, lng, state=None, prune_leading_zero=True):
     return block_group
 
 
+def point_to_block(lat, lng, state=None, prune_leading_zero=True):
+
+    url = ANM_CENSUS_INTERSECTION_ENDPOINT
+    payload = {}
+    headers = {}
+    params = {
+        'apiKey': ANM_KEY,
+        'version': ANM_PARSER_VERSION,
+        'censusYear': CENSUS_YEAR,
+        'lat': str(lat),
+        'lon': str(lng),
+        'format': 'json',
+    }
+
+    if state:
+        params['s'] = state
+
+    response, _id = safe_request.request(
+        API_NAME, "GET", url, headers=headers, data=payload, params=params, api_field='apiKey')
+
+    # first record is the actual result
+    try:
+        record = response['CensusRecords'][0]
+    except Exception:
+        utils.DB_REQUESTS[API_NAME].delete_one({'_id': _id})
+        print(Exception)
+        return None
+
+    # formula for census block as detailed here:
+    # https://www.census.gov/programs-surveys/geography/guidance/geo-identifiers.html
+    block = record['CensusStateFips'] + record['CensusCountyFips'] + \
+        record['CensusTract'] + record['CensusBlock']
+
+    block = block.replace('.', '')
+
+    if prune_leading_zero and block[0] == '0':
+        block = block[1:]
+
+    return block
+
+
 if __name__ == "__main__":
 
     def test_point_to_block_group():
-        print(point_to_block_group(34.056180, -118.276855, state='CA'))
+        print(point_to_block_group(34.056180, -118.296855, state='CA', prune_leading_zero=False))
 
-    test_point_to_block_group()
+    def test_point_to_block():
+        print(point_to_block(34.057278, -118.286941, state='CA', prune_leading_zero=False))
+        # 34.056180, -118.286855
+
+    test_point_to_block()
+    # test_point_to_block_group()
