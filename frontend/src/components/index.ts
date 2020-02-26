16d9c274cd90e13cbf076f3bd43b832516cc6bc8
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-02-26 22:35:56 +0700

    empty data container

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as CarouselFilter } from './filter/CarouselFilter';
+export { default as EmptyDataComponent } from './common/EmptyDataComponent';
 export { default as Filter } from './filter/Filter';
 export { default as MultiSelectBox } from './MultiSelectBox';
 export { default as RangeInput } from './filter/RangeInput';
