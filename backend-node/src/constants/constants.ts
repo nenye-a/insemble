15928fbae6f6a46449d86f0a1dc43751c25ae917
RaymondAnggara <raymond@kodefox.com>
2020-02-27 16:32:43 +0700

    Add s3 upload config on constant

--- backend-node/src/constants/constants.ts
+++ backend-node/src/constants/constants.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import AWS from 'aws-sdk';
 dotenv.config();
 
 let SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '';
@@ -6,4 +7,22 @@ let NODE_ENV = process.env.NODE_ENV || 'development';
 let HOST = process.env.HOST || 'http://localhost:4000'; // NOTES: make sure to set HOST on the env for production
 let FRONTEND_HOST = process.env.FRONTEND_HOST || 'http://localhost:8000'; // NOTES: make sure to set FRONTEND_HOST on the env for production
 
-export { HOST, NODE_ENV, SENDGRID_API_KEY, FRONTEND_HOST };
+const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET || '';
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  region: 'us-east-2',
+});
+
+type S3UploadResult = AWS.S3.ManagedUpload.SendData;
+
+export {
+  HOST,
+  NODE_ENV,
+  SENDGRID_API_KEY,
+  FRONTEND_HOST,
+  s3,
+  AWS_S3_BUCKET,
+  S3UploadResult,
+};
