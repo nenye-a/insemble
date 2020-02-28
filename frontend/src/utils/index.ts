389c83bef40a09769ac7d2f13147cf1a7bfdebbf
deandra <deandrasita@yahoo.com>
2020-02-28 14:56:59 +0700

    Deep Dive Key Facts Rounding (#273)
    * Round keyfacts
    
    * Minor adjustment
    
    * Nit
    
    * Adjustment
    
    * Format household income

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -4,5 +4,6 @@ export { default as useID } from './useID';
 export { default as useGoogleMaps } from './useGoogleMaps';
 export { default as roundDecimal } from './roundDecimal';
 export { default as formatGraphQLError } from './formatGraphQLError';
+export { default as getKeyfactsValue } from './getKeyfactsValue';
 export * from './authorization';
 export * from './isEqual';
