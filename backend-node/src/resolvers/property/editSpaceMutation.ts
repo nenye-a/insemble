53aa38b2c210bc3ab36a025ac9f4637b210e7b0a
RaymondAnggara <raymond@kodefox.com>
2020-02-28 14:06:02 +0700

    add upload function on editSpace

--- backend-node/src/resolvers/property/editSpaceMutation.ts
+++ backend-node/src/resolvers/property/editSpaceMutation.ts
@@ -1,21 +1,42 @@
 import { mutationField, arg, FieldResolver, stringArg } from 'nexus';
 
 import { Context } from 'serverTypes';
+import { uploadS3 } from '../../helpers/uploadUtils';
 
 export let editSpaceResolver: FieldResolver<'Mutation', 'editSpace'> = async (
   _,
   { space, spaceId },
   context: Context,
 ) => {
-  let { equipment = [], photos = [], available, ...spaceInput } = space || {};
+  let {
+    equipment = [],
+    photosUrl = [],
+    available,
+    mainPhoto,
+    photosUpload,
+    ...spaceInput
+  } = space || {};
+  if (!photosUrl) {
+    photosUrl = [];
+  }
+
+  if (photosUpload) {
+    for (let photoFile of photosUpload) {
+      let { Location: photoUrl } = await uploadS3(photoFile, 'MAIN_SPACE');
+      photosUrl.push(photoUrl);
+    }
+  }
+
+  let { Location: mainPhotoUrl } = await uploadS3(mainPhoto, 'MAIN_SPACE');
   let updatedSpace = await context.prisma.space.update({
     data: {
       ...spaceInput,
+      mainPhoto: mainPhotoUrl,
       equipment: {
         set: equipment,
       },
       photos: {
-        set: photos,
+        set: photosUrl,
       },
       available: new Date(available),
     },
