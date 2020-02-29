a3b6f6e6b4c63f054332a119a42a732332e1cb47
Raymond Anggara <46436058+RaymondAnggara@users.noreply.github.com>
2020-02-29 16:03:37 +0700

    Properties (#268)
    * Add type return for properties
    
    * Add properties endpoint
    
    * Add propreties endpoint to permission

---
+++ backend-node/src/resolvers/property/propertiesQuery.ts
@@ -0,0 +1,42 @@
+import { queryField, FieldResolver } from 'nexus';
+
+import { Root, Context } from 'serverTypes';
+
+let propertiesResolver: FieldResolver<'Query', 'properties'> = async (
+  _: Root,
+  _args,
+  context: Context,
+) => {
+  let properties = await context.prisma.property.findMany({
+    select: {
+      id: true,
+      propertyId: true,
+      name: true,
+      categories: true,
+      location: true,
+      space: true,
+    },
+    where: {
+      landlordUser: {
+        id: context.landlordUserId,
+      },
+    },
+  });
+
+  return properties.map((property) => {
+    let { space, ...restProperty } = property;
+    let stringDateSpace = space.map((sp) => {
+      let { available, ...restSpace } = sp;
+      return { ...restSpace, available: available.toString() };
+    });
+    return { ...restProperty, space: stringDateSpace };
+  });
+};
+
+let properties = queryField('properties', {
+  type: 'PropertyThumbnail',
+  resolve: propertiesResolver,
+  list: true,
+});
+
+export { properties };
