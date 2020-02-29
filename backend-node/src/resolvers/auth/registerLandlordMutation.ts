bf26c13faa462e4bbd08f1d403b4873fecb7f3f8
RaymondAnggara <raymond@kodefox.com>
2020-02-29 16:48:21 +0700

    remove create property from register

--- backend-node/src/resolvers/auth/registerLandlordMutation.ts
+++ backend-node/src/resolvers/auth/registerLandlordMutation.ts
@@ -8,10 +8,8 @@ export let registerLandlord = mutationField('registerLandlord', {
   type: 'LandlordRegisterResult',
   args: {
     landlord: arg({ type: 'LandlordRegisterInput', required: true }),
-    property: arg({ type: 'PropertyInput' }),
-    space: arg({ type: 'SpaceInput' }),
   },
-  resolve: async (_, { landlord, property, space }, context: Context, info) => {
+  resolve: async (_, { landlord }, context: Context) => {
     let password = bcrypt.hashSync(landlord.password, 10);
     let lowerCasedEmail = landlord.email.toLocaleLowerCase();
     let exist = await context.prisma.landlordUser.findMany({
