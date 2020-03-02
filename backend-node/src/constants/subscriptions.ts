df787af90363d20cc8edbab42d5f184beceb4264
Simon Sturmer <sstur@me.com>
2020-03-01 21:31:27 -0800

    Setup for Stripe and billing (#284)
    * Setup for Stripe and billing
    
    * Stripe Payment Handler

---
+++ backend-node/src/constants/subscriptions.ts
@@ -0,0 +1,9 @@
+import { TenantTier } from '@prisma/client';
+
+export const subscriptionPlans: Array<{
+  tier: TenantTier;
+  id: string;
+}> = [
+  { tier: 'EXPLORE', id: 'prod_GlgwI5jKh46guy' },
+  { tier: 'PROFESSIONAL', id: 'prod_GlgxaiFQlz2C0g' },
+];
