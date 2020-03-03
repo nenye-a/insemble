baab984b1dc1a0e7475b0304d661650312bb4371
Filbert <filbert@kodefox.com>
2020-03-03 14:57:25 +0700

    Fixed Based On Review

--- frontend/src/views/LandlordProfile/LandlordManageSpace.tsx
+++ frontend/src/views/LandlordProfile/LandlordManageSpace.tsx
@@ -2,8 +2,8 @@ import React, { useState, ChangeEvent } from 'react';
 import styled from 'styled-components';
 import { useQuery } from '@apollo/react-hooks';
 import { useForm, FieldError } from 'react-hook-form';
-import ReactFileReader from 'react-file-reader';
 
+import { GET_EQUIPMENT_LIST } from '../../graphql/queries/server/filters';
 import {
   View,
   RadioGroup,
@@ -15,64 +15,47 @@ import {
   Form,
   Text,
 } from '../../core-ui';
+import PhotosPicker from '../LandlordOnboardingPage/PhotosPicker';
+import { FileWithPreview } from '../../core-ui/Dropzone';
+import { Equipments } from '../../generated/Equipments';
+import SvgCircleClose from '../../components/icons/circle-close';
 import { THEME_COLOR, DARKER_GREY } from '../../constants/colors';
 import { PHOTOS } from '../../fixtures/dummyData';
-import { GET_EQUIPMENT_LIST } from '../../graphql/queries/server/filters';
-import { Equipments } from '../../generated/Equipments';
-import Placeholder from '../../assets/images/addphotos.png';
-import SvgCircleClose from '../../components/icons/circle-close';
 import { FONT_SIZE_MEDIUM, FONT_WEIGHT_BOLD, FONT_SIZE_SMALL } from '../../constants/theme';
 
-export default function LandlordManageSpace() {
+export default function LandlordSpace() {
   let { data: equipmentData, loading: equipmentLoading } = useQuery<Equipments>(GET_EQUIPMENT_LIST);
   let [description, setDescription] = useState<string>('');
   let [selectedCondition, setSelectedCondition] = useState('Whitebox');
   let [, setSelectedEquipment] = useState<Array<string>>([]);
   let { register, errors } = useForm();
   let [photos, setPhotos] = useState<Array<string | null>>(PHOTOS);
-  photos = PHOTOS;
-  if (photos.length < 5) {
+  let [mainPhoto, setMainPhoto] = useState<FileWithPreview | null>(null);
+  let [additionalPhotos, setAdditionalPhotos] = useState<Array<FileWithPreview | null>>([
+    null,
+    null,
+    null,
+    null,
+  ]);
+  while (photos.length < 5) {
     photos.push(null);
     setPhotos(photos);
   }
   let today = new Date().toISOString().slice(0, 10);
-  let onRemovePhoto = (index: number) => {
-    let newPhotos = photos.splice(index, 1, null);
-    setPhotos(newPhotos);
-  };
-  // let handleFiles = (files: string, index: number) => {
-  //   //TODO: Upload Photo when connect BE
-  // };
   return (
-    <Container>
-      <RowView>
-        <Title>Space 1</Title>
-        <TotalSpace>of 1</TotalSpace>
-      </RowView>
-      <LabelText text="Temporary Main Photo" />
+    <>
       <View flex>
-        <TouchableOpacity onPress={() => onRemovePhoto(0)}>
-          <CloseButton />
-        </TouchableOpacity>
-        <ReactFileReader handleFiles={() => {}}>
-          <Photo src={photos[0] == null ? Placeholder : photos[0]} />
-        </ReactFileReader>
+        <PhotosPicker
+          mainPhoto={mainPhoto}
+          onMainPhotoChange={(file: FileWithPreview | null) => {
+            setMainPhoto(file);
+          }}
+          additionalPhotos={additionalPhotos}
+          onAdditionalPhotoChange={(files: Array<FileWithPreview | null>) => {
+            setAdditionalPhotos(files);
+          }}
+        />
       </View>
-      <LabelText text="Additional Property Photos" />
-      <PhotosContainer flex>
-        {photos.slice(1).map((item, index) => (
-          <PhotoWrapper key={index} disabled={item != null} forwardedAs="button" type="button">
-            {item !== '' && (
-              <TouchableOpacity onPress={() => onRemovePhoto(index + 1)}>
-                <CloseButton />
-              </TouchableOpacity>
-            )}
-            <ReactFileReader handleFiles={() => {}}>
-              <Photos key={index} src={item == null ? Placeholder : item} alt={item || ''} />
-            </ReactFileReader>
-          </PhotoWrapper>
-        ))}
-      </PhotosContainer>
       <Form>
         <TextArea
           label="Description"
@@ -138,13 +121,9 @@ export default function LandlordManageSpace() {
           />
         </RowView>
       </Form>
-    </Container>
+    </>
   );
 }
-
-const Container = styled(View)`
-  padding: 24px 48px 24px 48px;
-`;
 
 const PhotosContainer = styled(View)`
   flex-wrap: wrap;
