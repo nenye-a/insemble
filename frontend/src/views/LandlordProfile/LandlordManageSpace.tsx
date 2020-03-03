34fdfbd16a6cfc25ee4fc020c8682077f45eee98
Filbert <filbert@kodefox.com>
2020-03-03 15:02:07 +0700

    Fixed Circle Warning

--- frontend/src/views/LandlordProfile/LandlordManageSpace.tsx
+++ frontend/src/views/LandlordProfile/LandlordManageSpace.tsx
@@ -11,17 +11,12 @@ import {
   TextArea,
   Label,
   MultiSelectInput,
-  TouchableOpacity,
   Form,
-  Text,
 } from '../../core-ui';
 import PhotosPicker from '../LandlordOnboardingPage/PhotosPicker';
 import { FileWithPreview } from '../../core-ui/Dropzone';
 import { Equipments } from '../../generated/Equipments';
-import SvgCircleClose from '../../components/icons/circle-close';
-import { THEME_COLOR, DARKER_GREY } from '../../constants/colors';
 import { PHOTOS } from '../../fixtures/dummyData';
-import { FONT_SIZE_MEDIUM, FONT_WEIGHT_BOLD, FONT_SIZE_SMALL } from '../../constants/theme';
 
 export default function LandlordSpace() {
   let { data: equipmentData, loading: equipmentLoading } = useQuery<Equipments>(GET_EQUIPMENT_LIST);
@@ -125,26 +120,6 @@ export default function LandlordSpace() {
   );
 }
 
-const PhotosContainer = styled(View)`
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-const Photos = styled.img`
-  height: 160px;
-  object-fit: cover;
-  border: 0.5px solid white;
-  border-radius: 5px;
-  width: 100%;
-`;
-const Photo = styled.img`
-  height: 300px;
-  object-fit: cover;
-  border: 0.5px solid white;
-  border-radius: 5px;
-  width: 100%;
-`;
-
 const RowView = styled(View)`
   flex-direction: row;
   align-items: center;
@@ -152,33 +127,12 @@ const RowView = styled(View)`
   margin: 24px 0 0 0;
 `;
 
-const PhotoWrapper = styled(TouchableOpacity)`
-  width: 24%;
-`;
 const LabelText = styled(Label)`
   margin: 24px 0 8px 0;
 `;
 
-const CloseButton = styled(SvgCircleClose)`
-  position: absolute;
-  top: 8px;
-  right: 8px;
-`;
-
 const RadioGroupContainer = styled(RadioGroup)`
   margin: 24px 0 0 0;
-`;
-
-const Title = styled(Text)`
-  font-size: ${FONT_SIZE_MEDIUM};
-  font-weight: ${FONT_WEIGHT_BOLD};
-  color: ${THEME_COLOR};
-  margin: 0 14px 0 0;
-`;
-const TotalSpace = styled(Text)`
-  font-size: ${FONT_SIZE_SMALL};
-  font-color: ${DARKER_GREY};
-  font-style: italic;
 `;
 
 const Features = styled(MultiSelectInput)`
