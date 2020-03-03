bf5fa5f37719d9007c13e1f7e1a2247653f9b2e7
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-03-03 18:33:49 +0700

    Show Avatar on Landlord Logged In (#308)
    * show avatar when landlord logged in
    
    * clear landlord token on logout
    
    * minor adjustment
    
    * show avatar on landing
    
    * guard landlord routes
    
    * adjust route

--- frontend/src/components/layout/HeaderNavigationBar.tsx
+++ frontend/src/components/layout/HeaderNavigationBar.tsx
@@ -1,23 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
-import { useQuery } from '@apollo/react-hooks';
+import { useLazyQuery } from '@apollo/react-hooks';
 
 import InsembleLogo from '../common/InsembleLogo';
 import { TouchableOpacity, Button, View, Avatar } from '../../core-ui';
 import { WHITE, HEADER_BORDER_COLOR, THEME_COLOR } from '../../constants/colors';
 import { NAVBAR_HEIGHT } from '../../constants/theme';
-import { GET_TENANT_PROFILE } from '../../graphql/queries/server/profile';
+import { GET_TENANT_PROFILE, GET_LANDLORD_PROFILE } from '../../graphql/queries/server/profile';
 import { GetTenantProfile } from '../../generated/GetTenantProfile';
+import { GetLandlordProfile } from '../../generated/GetLandlordProfile';
+import { asyncStorage } from '../../utils';
+import { Role } from '../../types/types';
 
 type Props = {
   showButton?: boolean;
 };
 
+type Profile = {
+  id: string;
+  avatar: string | null;
+};
+
 export default function HeaderNavigationBar(props: Props) {
   let history = useHistory();
-  let { data } = useQuery<GetTenantProfile>(GET_TENANT_PROFILE);
-  let avatar = data?.profileTenant.avatar;
+
+  let [profileInfo, setProfileInfo] = useState<Profile>({ id: '', avatar: '' });
+
+  let onTenantCompleted = (tenantResult: GetTenantProfile) => {
+    let { profileTenant } = tenantResult;
+    if (profileTenant) {
+      let { id, avatar } = profileTenant;
+      setProfileInfo({
+        id,
+        avatar,
+      });
+    }
+  };
+
+  let onLandlordCompleted = (landlordResult: GetLandlordProfile) => {
+    let { profileLandlord } = landlordResult;
+    if (profileLandlord) {
+      let { id, avatar } = profileLandlord;
+      setProfileInfo({
+        id,
+        avatar,
+      });
+    }
+  };
+
+  let [getTenantProfile] = useLazyQuery<GetTenantProfile>(GET_TENANT_PROFILE, {
+    onCompleted: onTenantCompleted,
+    fetchPolicy: 'network-only',
+  });
+
+  let [getLandlordProfile] = useLazyQuery<GetLandlordProfile>(GET_LANDLORD_PROFILE, {
+    onCompleted: onLandlordCompleted,
+    fetchPolicy: 'network-only',
+  });
+
+  useEffect(() => {
+    let role = asyncStorage.getRole();
+    if (role === Role.TENANT) {
+      getTenantProfile();
+    } else if (role === Role.LANDLORD) {
+      getLandlordProfile();
+    }
+  }, [getTenantProfile, getLandlordProfile]);
+
   return (
     <Container>
       <TouchableOpacity
@@ -27,13 +77,13 @@ export default function HeaderNavigationBar(props: Props) {
       >
         <InsembleLogo color="purple" />
       </TouchableOpacity>
-      {data?.profileTenant.id ? (
+      {profileInfo.id ? (
         <TouchableOpacity
           onPress={() => {
             history.push('/user/edit-profile');
           }}
         >
-          <Avatar size="small" image={avatar} />
+          <Avatar size="small" image={profileInfo.avatar || ''} />
         </TouchableOpacity>
       ) : (
         <>
