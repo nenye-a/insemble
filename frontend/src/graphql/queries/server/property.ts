ed1d53950c15f23c30678ee1d93caec8d67e3f0e
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-03-07 16:00:15 +0700

    Fix Create Property (#340)
    * fix create property type
    
    * convert date to ISO

--- frontend/src/graphql/queries/server/property.ts
+++
@@ -1,7 +0,0 @@
-import gql from 'graphql-tag';
-
-export const CREATE_PROPERTY = gql`
-  mutation CreateProperty($property: PropertyInput!, $space: CreateSpaceInput!) {
-    createProperty(property: $property, space: $space)
-  }
-`;
