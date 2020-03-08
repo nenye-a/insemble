d2655867b2107269522ad3ba8b6ca572ca115338
Joshua <joshuasetiawan04@gmail.com>
2020-03-08 12:10:21 +0700

    update keyfacts helper to nullable

--- frontend/src/utils/getKeyfactsValue.ts
+++ frontend/src/utils/getKeyfactsValue.ts
@@ -1,6 +1,6 @@
 import { convertToKilos } from '.';
 
-export default function getKeyfactsValue(val?: number) {
+export default function getKeyfactsValue(val?: number | null) {
   if (val && val >= 10000) {
     let formatted = convertToKilos(val).split('.')[0] + 'K';
     return formatted;
