5c2a64264afa834a1f10cc08b0ae0f4449982e17
Deandra Sita <deandrasita@yahoo.com>
2020-03-09 15:37:23 +0700

    Adjustment

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -22,10 +22,7 @@ export default function Login() {
         titleBackground="purple"
       >
         <FormContainer>
-          <LoginForm
-            role={Role.TENANT}
-            onboardingState={history.location.state && history.location.state.onboardingState}
-          />
+          <LoginForm role={Role.TENANT} onboardingState={history.location.state?.onboardingState} />
         </FormContainer>
       </LoginCard>
       <NoAccountContainer>
