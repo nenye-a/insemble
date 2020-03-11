e113c14f368b09a3d19aac73089c173922e4d5f9
Deandra Sita <deandrasita@yahoo.com>
2020-03-11 11:44:56 +0700

    Reset password

---
+++ frontend/src/views/ResetPasswordTenant.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Card, View } from '../core-ui';
+import { FONT_SIZE_MEDIUM, FONT_WEIGHT_NORMAL } from '../constants/theme';
+import ResetPasswordForm from './ResetPasswordForm';
+import { Role } from '../types/types';
+
+export default function ResetPasswordTenant() {
+  return (
+    <Container>
+      <ContainerCard
+        titleContainerProps={{ style: { textAlign: 'center', height: 54 } }}
+        title="New Password"
+        titleProps={{ style: { fontSize: FONT_SIZE_MEDIUM, fontWeight: FONT_WEIGHT_NORMAL } }}
+        titleBackground="purple"
+      >
+        <ResetPasswordForm role={Role.TENANT} />
+      </ContainerCard>
+    </Container>
+  );
+}
+
+const Container = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ContainerCard = styled(Card)`
+  width: 400px;
+  height: fit-content;
+`;
