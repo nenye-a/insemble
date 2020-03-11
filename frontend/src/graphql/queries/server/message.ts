6b42a8584f4bd638f5d3c39675170039c6f665e2
Deandra Sita <deandrasita@yahoo.com>
2020-03-11 13:35:53 +0700

    Landlord message scene

---
+++ frontend/src/graphql/queries/server/message.ts
@@ -0,0 +1,25 @@
+import gql from 'graphql-tag';
+
+export const CREATE_CONVERSATION = gql`
+  mutation CreateConversation(
+    $brandId: String!
+    $propertyId: String!
+    $matchScore: Int!
+    $header: String!
+    $messageInput: MessageInput!
+  ) {
+    createConversation(
+      brandId: $brandId
+      propertyId: $propertyId
+      matchScore: $matchScore
+      header: $header
+      messageInput: $messageInput
+    )
+  }
+`;
+
+export const SEND_MESSAGE = gql`
+  mutation SendMessage($conversationId: String!, $messageInput: MessageInput!) {
+    sendMessage(conversationId: $conversationId, messageInput: $messageInput)
+  }
+`;
