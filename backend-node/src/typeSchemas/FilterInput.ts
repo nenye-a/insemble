7ab7065f976226acde5b9f23fc719d6dc0465881
RaymondAnggara <raymond@kodefox.com>
2020-03-18 11:15:18 +0700

    Add maxSize to input

--- backend-node/src/typeSchemas/FilterInput.ts
+++ backend-node/src/typeSchemas/FilterInput.ts
@@ -8,6 +8,7 @@ export let FilterInput = inputObjectType({
     t.int('minRent');
     t.int('maxRent');
     t.int('minSize');
+    t.int('maxSize');
     t.int('minFrontageWidth');
     t.int('minAge');
     t.int('maxAge');
