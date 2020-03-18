9415be6a18f9291c07071c1d5f90a53cf1a00c03
Raymond Anggara <46436058+RaymondAnggara@users.noreply.github.com>
2020-03-18 17:00:42 +0700

    Add propertyQuery (#454)

---
+++ backend-node/src/resolvers/property/propertyQuery.ts
@@ -0,0 +1,29 @@
+import { queryField, FieldResolver, stringArg } from 'nexus';
+
+import { Root, Context } from 'serverTypes';
+
+let propertyResolver: FieldResolver<'Query', 'property'> = async (
+  _: Root,
+  { propertyId },
+  context: Context,
+) => {
+  let property = await context.prisma.property.findOne({
+    where: {
+      id: propertyId,
+    },
+    include: { location: true, space: true },
+  });
+  if (!property) {
+    throw new Error('Property not found!');
+  }
+
+  return property;
+};
+
+let property = queryField('property', {
+  type: 'Property',
+  resolve: propertyResolver,
+  args: { propertyId: stringArg({ required: true }) },
+});
+
+export { property };
