9fd3358fdd3272a10ec7500439dccebd69da065d
RaymondAnggara <raymond@kodefox.com>
2020-03-18 10:58:44 +0700

    Update brand and matching property type scheema

--- backend-node/src/typeSchemas/MatchingProperty.ts
+++ backend-node/src/typeSchemas/MatchingProperty.ts
@@ -6,6 +6,14 @@ export let MatchingProperty = objectType({
     t.string('address');
     t.int('rent');
     t.int('sqft');
-    t.string('type');
+    t.string('type', { list: true });
+    t.string('spaceId');
+    t.string('spaceCondition', { list: true });
+    t.string('tenantType', { list: true });
+    t.boolean('pro');
+    t.boolean('visible');
+    t.string('lat');
+    t.string('lng');
+    t.float('matchValue');
   },
 });
