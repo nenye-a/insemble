23d559f9e28c19d9391b9f71076b8ee1ce34cb94
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-03-19 15:00:07 +0700

    Fix Click on Location Preview (#437)
    * add missing type on infobox
    
    * fix location preview click listener

--- frontend/src/views/MapContainer.tsx
+++ frontend/src/views/MapContainer.tsx
@@ -52,7 +52,6 @@ function MapContainer({ onMarkerClick, matchingLocations }: Props) {
   let [markerPosition, setMarkerPosition] = useState<LatLng | null>(null);
   let [showGuide, setShowGuide] = useState(history.location.state?.newBrand);
   let [infoBoxHeight, setInfoBoxHeight] = useState<number>(0);
-  let [domReady, setDomReady] = useState(false);
 
   let infoRef = useRef<Element | undefined>();
   let mapRef = useRef<GoogleMap | null>(null);
@@ -75,8 +74,11 @@ function MapContainer({ onMarkerClick, matchingLocations }: Props) {
   };
 
   let onMapClick = async (latLng: LatLng) => {
+    // setMarkerPosition(null);
     let { lat, lng } = latLng;
-
+    // if (markerPosition) {
+    //   setMarkerPosition(null);
+    // }
     getLocation({
       variables: {
         brandId,
@@ -88,24 +90,12 @@ function MapContainer({ onMarkerClick, matchingLocations }: Props) {
       },
     });
     setMarkerPosition(latLng);
-    setDomReady(false);
   };
 
-  useEffect(() => {
-    let handlePreviewClickListener = (e: Event) => {
-      e.stopPropagation();
-      onPreviewClick();
-    };
-
-    if (infoRef.current && domReady) {
-      infoRef.current.addEventListener('click', handlePreviewClickListener);
-      return () => {
-        if (infoRef.current) {
-          infoRef.current.removeEventListener('click', handlePreviewClickListener);
-        }
-      };
-    }
-  });
+  let handlePreviewClickListener = (e: Event) => {
+    e.stopPropagation();
+    onPreviewClick();
+  };
 
   useEffect(() => {
     if (outsideBoundError) {
@@ -161,14 +151,17 @@ function MapContainer({ onMarkerClick, matchingLocations }: Props) {
               onDomReady={() => {
                 let infoBox = document.querySelector('.infoBox');
                 if (infoBox) {
-                  setDomReady(true);
                   infoRef.current = infoBox;
+                  infoRef.current.addEventListener('click', handlePreviewClickListener);
                   let infoBoxHeight = infoBox.getClientRects()[0].height;
                   setInfoBoxHeight(infoBoxHeight);
                 }
               }}
               onCloseClick={() => {
                 setMarkerPosition(null);
+              }}
+              onUnmount={() => {
+                infoRef.current?.removeEventListener('click', handlePreviewClickListener);
               }}
             >
               <LocationDetail
