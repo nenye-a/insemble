c0cb451fe7102399160e44a85b73ea400546d2cd
RaymondAnggara <raymond@kodefox.com>
2020-03-20 13:31:29 +0700

    Add contact Type

--- backend-node/src/typeSchemas/ReceiverContact.ts
+++ backend-node/src/typeSchemas/ReceiverContact.ts
@@ -1,11 +1,21 @@
-import { inputObjectType } from 'nexus';
+import { inputObjectType, objectType } from 'nexus';
 
-export let ReceiverContact = inputObjectType({
-  name: 'ReceiverContact',
+export let ReceiverContactInput = inputObjectType({
+  name: 'ReceiverContactInput',
   definition(t) {
     t.string('name', { required: true });
     t.string('email', { required: true });
     t.string('phone', { required: true });
     t.string('role', { required: true });
+  },
+});
+
+export let ReceiverContact = objectType({
+  name: 'ReceiverContact',
+  definition(t) {
+    t.string('name');
+    t.string('email');
+    t.string('phone');
+    t.string('role');
   },
 });
