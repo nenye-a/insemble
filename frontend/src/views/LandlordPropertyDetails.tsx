105d6b339cf53a35a2832d1f8b3402797bddb087
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-03-20 12:29:08 +0700

    fix test

--- frontend/src/views/LandlordPropertyDetails.tsx
+++ frontend/src/views/LandlordPropertyDetails.tsx
@@ -69,7 +69,7 @@ export default function LandlordPropertyDetails() {
     <View flex>
       <PropertyDetailHeader
         spaces={spaces}
-        address={address}
+        address={property.location.address}
         request="1" // TODO
         selectedSpaceIndex={selectedSpaceIndex}
         onPressSpace={(index: number) => {
@@ -79,7 +79,7 @@ export default function LandlordPropertyDetails() {
         onPressAdd={() => {
           history.push(`/landlord/add-space/step-1`, {
             propertyId: params.paramsId,
-            address,
+            address: property.location.address,
           });
         }}
       />
