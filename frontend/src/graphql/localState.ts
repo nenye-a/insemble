5ac8b17dfe85424e8bf9f8370af9012950643b76
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-04-02 23:40:59 +0700

    adjust default state for tenant onboarding

--- frontend/src/graphql/localState.ts
+++ frontend/src/graphql/localState.ts
@@ -1,3 +1,7 @@
+import { LocationInput } from '../generated/globalTypes';
+
+type WithTypename = { __typename: string };
+
 export type UserContent = {
   token: string;
   email: string;
@@ -19,7 +23,68 @@ export type ErrorState = {
   errorState: { __typename: string } & ErrorContent;
 };
 
-export type RootState = UserState & ErrorState;
+// TODO: clean up
+export enum NewLocationPlan {
+  YES = 'YES',
+  NOT_ACTIVE = 'NOT_ACTIVE',
+  NOT_PLANNING = 'NOT_PLANNING',
+}
+
+export type NewLocationPlanObj = {
+  label: string;
+  value: NewLocationPlan;
+};
+
+export type ConfirmBusinessDetail = WithTypename & {
+  name: string;
+  location?: LocationInput;
+  categories: Array<string>;
+  userRelation: string;
+  otherUserRelation?: string;
+};
+
+export type TenantGoals = WithTypename & {
+  newLocationPlan?: NewLocationPlanObj;
+  location: Array<LocationInput>;
+  locationCount?: string;
+};
+
+export type TargetCustomers = WithTypename & {
+  minAge?: number;
+  maxAge?: number;
+  noAgePreference?: boolean;
+  minIncome?: number;
+  maxIncome?: number;
+  personas?: Array<string>;
+  noPersonasPreference?: boolean;
+  educations?: Array<string>;
+  noEducationsPreference?: boolean;
+  minDaytimePopulation?: string;
+  noMinDaytimePopulationPreference?: boolean;
+};
+
+export type PhysicalSiteCriteria = WithTypename & {
+  minSize?: number | string;
+  maxSize?: number | string;
+  minFrontageWidth?: number | string;
+  equipments?: Array<string>;
+  spaceType?: Array<string>;
+};
+
+type TenantOnboardingContent = {
+  confirmBusinessDetail: ConfirmBusinessDetail;
+  tenantGoals: TenantGoals;
+  targetCustomers: TargetCustomers;
+  physicalSiteCriteria: PhysicalSiteCriteria;
+};
+
+type TenantOnboardingState = {
+  tenantOnboardingState: {
+    __typename: string;
+  } & TenantOnboardingContent;
+};
+
+export type RootState = UserState & ErrorState & TenantOnboardingState;
 
 export const defaultState: RootState = {
   userState: {
@@ -36,5 +101,25 @@ export const defaultState: RootState = {
   errorState: {
     __typename: 'ErrorState',
     locationPreview: false,
+  },
+  tenantOnboardingState: {
+    __typename: 'TenantOnboardingState',
+    confirmBusinessDetail: {
+      __typename: 'TenantOnboardingConfirmBusinessDetail',
+      name: '',
+      categories: [],
+      userRelation: '',
+    },
+    tenantGoals: {
+      __typename: 'TenantOnboardingTenantGoals',
+      newLocationPlan: undefined,
+      location: [],
+    },
+    targetCustomers: {
+      __typename: 'TenantOnboardingTargetCustomers',
+    },
+    physicalSiteCriteria: {
+      __typename: 'TenantOnboardingPhysicalSiteCriteria',
+    },
   },
 };
