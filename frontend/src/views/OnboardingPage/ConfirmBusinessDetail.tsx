2a618ee207cb58be787b4f9014841075358ea8b6
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-04-03 12:26:46 +0700

    refactor confirm business detail

--- frontend/src/views/OnboardingPage/ConfirmBusinessDetail.tsx
+++ frontend/src/views/OnboardingPage/ConfirmBusinessDetail.tsx
@@ -1,7 +1,7 @@
-import React, { useState, Dispatch, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useLazyQuery, useMutation } from '@apollo/react-hooks';
 import { useForm, FieldError, FieldValues } from 'react-hook-form';
 
 import {
@@ -14,6 +14,7 @@ import {
   ClickAway,
   Form,
   PillButton,
+  LoadingIndicator,
 } from '../../core-ui';
 import { Filter } from '../../components';
 import { BUTTON_TRANSPARENT_TEXT_COLOR, RED_TEXT } from '../../constants/colors';
@@ -21,52 +22,40 @@ import { MAPS_IFRAME_URL_SEARCH } from '../../constants/googleMaps';
 import { FONT_SIZE_SMALL } from '../../constants/theme';
 import { GET_CATEGORIES, GET_AUTOPOPULATE_FILTER } from '../../graphql/queries/server/filters';
 import { Categories } from '../../generated/Categories';
-import { Action, State as OnboardingState } from '../../reducers/tenantOnboardingReducer';
 import {
   AutoPopulateFilter,
   AutoPopulateFilterVariables,
 } from '../../generated/AutoPopulateFilter';
 import { LocationState } from './types';
 import OnboardingFooter from '../../components/layout/OnboardingFooter';
+import {
+  GET_TENANT_ONBOARDING_STATE,
+  UPDATE_TENANT_ONBOARDING,
+} from '../../graphql/queries/client/tenantOnboarding';
+import { TenantOnboardingState } from '../../graphql/localState';
+import { LocationInput } from '../../generated/globalTypes';
 
-type Props = {
-  dispatch: Dispatch<Action>;
-  state: OnboardingState;
-};
-
-export default function ConfirmBusinessDetail(props: Props) {
-  let { dispatch, state: onboardingState } = props;
-  let { confirmBusinessDetail } = onboardingState;
-
+export default function ConfirmBusinessDetail() {
   let { register, errors, handleSubmit } = useForm();
   let { data: categoriesData } = useQuery<Categories>(GET_CATEGORIES);
-  let { data: autopopulateData, loading: autopopulateLoading } = useQuery<
-    AutoPopulateFilter,
-    AutoPopulateFilterVariables
-  >(GET_AUTOPOPULATE_FILTER, {
-    variables: {
-      address: confirmBusinessDetail.location?.address || '',
-      brandName: confirmBusinessDetail.name,
-    },
-    skip: !confirmBusinessDetail.location?.address || !confirmBusinessDetail.name,
-  });
-  let [selectedBusinessRelation, setBussinesRelation] = useState(
-    confirmBusinessDetail.userRelation || ''
-  );
+  let [
+    getAutopopulateData,
+    { data: autopopulateData, loading: autopopulateLoading, error: apError },
+  ] = useLazyQuery<AutoPopulateFilter, AutoPopulateFilterVariables>(GET_AUTOPOPULATE_FILTER);
+  let { data: onboardingStateData, loading: onboardingStateLoading } = useQuery<
+    TenantOnboardingState
+  >(GET_TENANT_ONBOARDING_STATE);
+  let [updateTenantOnboarding] = useMutation(UPDATE_TENANT_ONBOARDING);
+  let [selectedBusinessRelation, setBussinesRelation] = useState('');
   let [categorySelectionVisible, toggleCategorySelection] = useState(false);
-  let [selectedCategories, setSelectedCategories] = useState<Array<string>>(
-    confirmBusinessDetail.categories || []
-  );
+  let [selectedCategories, setSelectedCategories] = useState<Array<string>>([]);
+  let [selectedLocation, setSelectedLocation] = useState<LocationInput | null>(null);
   let [isSubmitted, setSubmitted] = useState(false);
   let history = useHistory<LocationState>();
 
   let iframeSource =
-    confirmBusinessDetail.location?.lat && confirmBusinessDetail.location?.lng
-      ? MAPS_IFRAME_URL_SEARCH +
-        '&q=' +
-        confirmBusinessDetail.location?.lat +
-        ', ' +
-        confirmBusinessDetail.location?.lng
+    selectedLocation?.lat && selectedLocation?.lng
+      ? MAPS_IFRAME_URL_SEARCH + '&q=' + selectedLocation.lat + ', ' + selectedLocation.lng
       : '';
 
   useEffect(() => {
@@ -75,12 +64,32 @@ export default function ConfirmBusinessDetail(props: Props) {
     }
   }, [autopopulateData]);
 
+  useEffect(() => {
+    if (onboardingStateData) {
+      let {
+        name,
+        location: { lat, lng, address },
+        categories,
+        userRelation,
+      } = onboardingStateData.tenantOnboardingState.confirmBusinessDetail;
+      setBussinesRelation(userRelation);
+      setSelectedCategories(categories);
+      setSelectedLocation({ lat, lng, address });
+      if (address && name) {
+        getAutopopulateData({
+          variables: {
+            address: address,
+            brandName: name,
+          },
+        });
+      }
+    }
+  }, [onboardingStateData, getAutopopulateData]);
+
   let saveFormState = (fieldValues: FieldValues) => {
-    dispatch({
-      type: 'SAVE_CHANGES_CONFIRM_BUSINESS_DETAIL',
-      values: {
+    updateTenantOnboarding({
+      variables: {
         confirmBusinessDetail: {
-          ...onboardingState.confirmBusinessDetail,
           name: fieldValues.businessName,
           categories: selectedCategories,
           userRelation: fieldValues.businessRelationship,
@@ -99,114 +108,124 @@ export default function ConfirmBusinessDetail(props: Props) {
     }
   };
 
-  return (
-    <Form
-      style={{ flex: 1 }}
-      onSubmit={() => {
-        if (!isSubmitted) {
-          setSubmitted(true);
-        }
-        handleSubmit(onSubmit)();
-      }}
-    >
-      {iframeSource && <Iframe src={iframeSource} />}
-      <FormContainer>
-        <TextInput
-          label="Business Name"
-          defaultValue={confirmBusinessDetail.name}
-          name="businessName"
-          ref={register({
-            required: 'Business Name should not be empty',
-          })}
-          errorMessage={(errors?.businessName as FieldError)?.message || ''}
-          containerStyle={{ paddingBottom: 12 }}
-        />
-        <CategoryInput>
-          <RowedView>
-            <Label text="Categories" />
-            <EditButton
-              text="Edit"
-              onPress={() => toggleCategorySelection(!categorySelectionVisible)}
-            />
-          </RowedView>
-          <ClickAway onClickAway={() => toggleCategorySelection(false)} style={{ zIndex: 1 }}>
-            {categoriesData && (
-              <FilterContainer
-                search
-                visible={categorySelectionVisible}
-                selectedOptions={selectedCategories}
-                allOptions={categoriesData.categories}
-                onSelect={(category: string) => {
-                  setSelectedCategories([...selectedCategories, category]);
-                }}
-                onUnSelect={(category: string) => {
-                  let newSelectedCategories = selectedCategories.filter(
-                    (el: string) => !el.includes(category)
-                  );
-                  setSelectedCategories(newSelectedCategories);
-                }}
-                onDone={() => toggleCategorySelection(false)}
-                loading={autopopulateLoading}
-              />
-            )}
-          </ClickAway>
-          <RowWrap>
-            {selectedCategories.map((category, index) => (
-              <PillButton primary key={index} style={{ marginRight: 4, marginTop: 4 }}>
-                {category}
-              </PillButton>
-            ))}
-          </RowWrap>
-          {isSubmitted && selectedCategories.length === 0 && (
-            <ErrorText>Please Select Categories</ErrorText>
-          )}
-        </CategoryInput>
-
-        <LabelText text="What is your relationship to this business?" />
-        <RadioGroup
-          name="businessRelationship"
-          options={[
-            'I am the owner of this business',
-            'I am a development manager',
-            'I am an agent representing this business',
-            'Other',
-          ]}
-          selectedOption={selectedBusinessRelation}
-          onSelect={(item) => {
-            setBussinesRelation(item);
-          }}
-          radioItemProps={{
-            style: { marginBottom: 8 },
-            ref: register({
-              required: 'Please select your relation to this business',
-            }),
-          }}
-          errorMessage={(errors?.businessRelationship as FieldError)?.message || ''}
-        />
-        {/* TODO: put to constants */}
-        {selectedBusinessRelation === 'Other' && (
-          <OtherTextInput
-            placeholder="Landlord"
+  if (!onboardingStateLoading && onboardingStateData) {
+    let {
+      name,
+      otherUserRelation,
+    } = onboardingStateData.tenantOnboardingState.confirmBusinessDetail;
+    return (
+      <Form
+        style={{ flex: 1 }}
+        onSubmit={() => {
+          if (!isSubmitted) {
+            setSubmitted(true);
+          }
+          handleSubmit(onSubmit)();
+        }}
+      >
+        {iframeSource && <Iframe src={iframeSource} />}
+        <FormContainer>
+          <TextInput
+            label="Business Name"
+            defaultValue={name}
+            name="businessName"
             ref={register({
-              required: 'Input should not be empty',
+              required: 'Business Name should not be empty',
             })}
-            name="otherBusinessRelation"
-            errorMessage={(errors?.otherBusinessRelation as FieldError)?.message || ''}
-            defaultValue={onboardingState.confirmBusinessDetail.otherUserRelation}
+            errorMessage={(errors?.businessName as FieldError)?.message || ''}
+            containerStyle={{ paddingBottom: 12 }}
           />
-        )}
-      </FormContainer>
-      <OnboardingFooter>
-        <TransparentButton
-          text="Not My Address"
-          mode="transparent"
-          type="submit"
-          onPress={() => history.goBack()}
-        />
-        <Button text="Next" type="submit" />
-      </OnboardingFooter>
-    </Form>
-  );
+          <CategoryInput>
+            <RowedView>
+              <Label text="Categories" />
+              <EditButton
+                text="Edit"
+                onPress={() => toggleCategorySelection(!categorySelectionVisible)}
+              />
+            </RowedView>
+            <ClickAway onClickAway={() => toggleCategorySelection(false)} style={{ zIndex: 1 }}>
+              {categoriesData && (
+                <FilterContainer
+                  search
+                  visible={categorySelectionVisible}
+                  selectedOptions={selectedCategories}
+                  allOptions={categoriesData.categories}
+                  onSelect={(category: string) => {
+                    setSelectedCategories([...selectedCategories, category]);
+                  }}
+                  onUnSelect={(category: string) => {
+                    let newSelectedCategories = selectedCategories.filter(
+                      (el: string) => !el.includes(category)
+                    );
+                    setSelectedCategories(newSelectedCategories);
+                  }}
+                  onDone={() => toggleCategorySelection(false)}
+                  loading={autopopulateLoading}
+                />
+              )}
+            </ClickAway>
+            <RowWrap>
+              {selectedCategories.map((category, index) => (
+                <PillButton primary key={index} style={{ marginRight: 4, marginTop: 4 }}>
+                  {category}
+                </PillButton>
+              ))}
+            </RowWrap>
+            {isSubmitted && selectedCategories.length === 0 && (
+              <ErrorText>Please Select Categories</ErrorText>
+            )}
+          </CategoryInput>
+
+          <LabelText text="What is your relationship to this business?" />
+          <RadioGroup
+            name="businessRelationship"
+            options={[
+              'I am the owner of this business',
+              'I am a development manager',
+              'I am an agent representing this business',
+              'Other',
+            ]}
+            selectedOption={selectedBusinessRelation}
+            onSelect={(item) => {
+              setBussinesRelation(item);
+            }}
+            radioItemProps={{
+              style: { marginBottom: 8 },
+              ref: register({
+                required: 'Please select your relation to this business',
+              }),
+            }}
+            errorMessage={(errors?.businessRelationship as FieldError)?.message || ''}
+          />
+          {/* TODO: put to constants */}
+          {selectedBusinessRelation === 'Other' && (
+            <OtherTextInput
+              placeholder="Landlord"
+              ref={register({
+                required: 'Input should not be empty',
+              })}
+              name="otherBusinessRelation"
+              errorMessage={(errors?.otherBusinessRelation as FieldError)?.message || ''}
+              defaultValue={otherUserRelation}
+            />
+          )}
+        </FormContainer>
+        <OnboardingFooter>
+          <TransparentButton
+            text="Not My Address"
+            mode="transparent"
+            type="submit"
+            onPress={() => history.goBack()}
+          />
+          <Button text="Next" type="submit" />
+        </OnboardingFooter>
+      </Form>
+    );
+  } else if (onboardingStateLoading) {
+    return <LoadingIndicator />;
+  } else {
+    return null;
+  }
 }
 
 const Iframe = styled.iframe`
