21f850f45f0d7fa0eb85b9160fe334eabee83807
Filbert <filbert@kodefox.com>
2020-04-14 13:13:55 +0700

    Add Space Plan Scene

---
+++ frontend/src/views/LandlordBilling.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import { THEME_COLOR } from '../constants/colors';
+import { Text, Card, View, SegmentedControl } from '../core-ui';
+import LandlordPLan from './LandlordPlan';
+import { FONT_SIZE_LARGE, FONT_WEIGHT_MEDIUM } from '../constants/theme';
+
+export default function LandlordBilling() {
+  let [selectedTab, setSelectedTab] = useState(0);
+
+  return (
+    <Container>
+      <Title>Billing & Plans</Title>
+      <Tab
+        containerStyle={{ borderWidth: 0, height: 36 }}
+        options={['Space Plans', 'Billing Summary']}
+        selectedIndex={selectedTab}
+        onPress={(index: number) => {
+          setSelectedTab(index);
+        }}
+      />
+
+      {selectedTab === 0 ? <LandlordPLan /> : <View></View>}
+    </Container>
+  );
+}
+
+const Container = styled(Card)`
+  padding: 24px;
+`;
+const Tab = styled(SegmentedControl)`
+  width: 240px;
+`;
+const Title = styled(Text)`
+  margin-bottom: 20px;
+  font-size: ${FONT_SIZE_LARGE};
+  font-weight: ${FONT_WEIGHT_MEDIUM};
+  color: ${THEME_COLOR};
+`;
