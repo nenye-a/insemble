8a683e7be7bf09cbe228b5ccc8f74c13a1678ec7
Nenye <57047007+nenye-a@users.noreply.github.com>
2020-04-28 11:29:03 -0700

    update installed packages and run autopep

--- data_testing/scripts/run_logos.py
+++ data_testing/scripts/run_logos.py
@@ -8,13 +8,14 @@ Run file to get logos of all retailers in the database
 
 '''
 
+
 def run_logos():
     """
 
     Generates all of the logos for all of the retailers in the database
 
     """
-    #get retailer names sorted by num user ratings overall
+    # get retailer names sorted by num user ratings overall
     #names = get_unique_names()
 
     batch_size = {'size': 100}
@@ -28,8 +29,8 @@ def run_logos():
     while getting_logos:
 
         print(
-                "**BB - Starting new batch of {}".format(batch_size['size'])
-            )
+            "**BB - Starting new batch of {}".format(batch_size['size'])
+        )
         spaces = utils.DB_PROCESSED_SPACE.aggregate([
             {'$match': query},
             {'$sample': batch_size}
@@ -42,7 +43,7 @@ def run_logos():
 
             if not logo:
                 print(
-                "**BB - Could not resolve logo for {}".format(name)
+                    "**BB - Could not resolve logo for {}".format(name)
                 )
                 continue
 
@@ -56,24 +57,26 @@ def run_logos():
                 "**BB - Run Logo: {} documents updated for {} with logo {}".format(x.modified_count, name, logo)
             )
 
+
 def get_unique_names():
-    #get retailer names from database and add to dictionary of names, # reviews
-    items = utils.DB_SPACE.spaces.find({}, {"name":1, "user_ratings_total":1, "_id":0})
+    # get retailer names from database and add to dictionary of names, # reviews
+    items = utils.DB_SPACE.spaces.find({}, {"name": 1, "user_ratings_total": 1, "_id": 0})
     # count = 0
     names = {}
     for item in items:
-        try: 
-            names[item["name"]] = names[item["name"]]+item["user_ratings_total"]
-        except: 
+        try:
+            names[item["name"]] = names[item["name"]] + item["user_ratings_total"]
+        except:
             try:
                 names[item["name"]] = item["user_ratings_total"]
-            except: 
+            except:
                 names[item["name"]] = 0
         # count += 1
         # if count % 2000 == 0:
-        #     print("counted", count, "database entries") 
+        #     print("counted", count, "database entries")
 
     return {k: v for k, v in sorted(names.items(), key=lambda item: item[1], reverse=True)}
+
 
 def get_logo(inp_name):
     """
@@ -87,20 +90,19 @@ def get_logo(inp_name):
         return None
     return response["logo"]
 
+
 if __name__ == "__main__":
     def test_get_names_logos():
         names = get_unique_names()
         print("total entries", len(names))
-        names_list= list(names.keys())[:30]
+        names_list = list(names.keys())[:30]
         start = time.time()
-        for name in names_list: 
+        for name in names_list:
             logo = get_logo(name)
-            #print(logo)
-        print("time lapse", time.time()-start)
+            # print(logo)
+        print("time lapse", time.time() - start)
 
         #print("first entry", first_name,names[first_name])
         #print("subway numbers", names["Subway"])
-    
+
     run_logos()
-    
-    
