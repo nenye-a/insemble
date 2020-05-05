841f44fdacec8fbf02ff26aa8a748824db99824f
Filbert <39455112+FilbertAW@users.noreply.github.com>
2020-05-05 20:36:04 +0700

    Disabled Brand If User Out Of Trial (#707)

--- frontend/src/views/TenantBrands.tsx
+++ frontend/src/views/TenantBrands.tsx
@@ -89,7 +89,7 @@ export default () => {
       ) : (
         data &&
         data.brands.map((brandData, index) => {
-          let { name, categories, matchingLocations, id } = brandData;
+          let { name, categories, matchingLocations, id, locked } = brandData;
           let heatmapData = ((matchingLocations
             ? matchingLocations.map(({ lat, lng, match }) => {
                 return {
@@ -100,7 +100,7 @@ export default () => {
             : []) as unknown) as google.maps.MVCArray<google.maps.visualization.WeightedLocation>;
           return (
             <View key={index}>
-              <TouchableOpacity style={{ marginBottom: 24 }}>
+              <TouchableOpacity style={{ marginBottom: 24 }} disabled={locked}>
                 <HistoryContainer>
                   <LeftContainer
                     flex
