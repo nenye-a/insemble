5be66906564ece945073133214ad0412de1b3f7f
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-13 19:41:22 +0700

    setup sentry

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({ dsn: 'https://0aabc7f8c2ec40fd8833af5d43f6358e@o392065.ingest.sentry.io/5239064' });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
