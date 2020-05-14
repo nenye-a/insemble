7011bbd9dbf7dabb424f2c9a4d522a6f46196da6
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-05-14 17:21:33 +0700

    Setup Sentry for Error Tracking (#745)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+Sentry.init({ dsn: 'DELETED_URL_WITH_CREDENTIALS
